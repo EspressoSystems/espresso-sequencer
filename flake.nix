c4c176f73c69c5f3a99aa9d95535ab24804aaffa
sveitser <sveitser@gmail.com>
2022-11-10 16:21:06 +0100

    Add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "espresso-evm";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.flake-compat.url = "github:edolstra/flake-compat";
+  inputs.flake-compat.flake = false;
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, flake-compat, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell
+          {
+            buildInputs = [
+              graphviz
+              plantuml
+            ];
+          };
+      }
+    );
+}
