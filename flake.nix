9777f746a314ab1051ebfa4f12b88863418ba08e
sveitser <sveitser@gmail.com>
2022-11-11 12:20:16 +0100

    Add pre-commit hook to generate diagram

--- flake.nix
+++ flake.nix
@@ -10,7 +10,9 @@
   inputs.flake-compat.url = "github:edolstra/flake-compat";
   inputs.flake-compat.flake = false;
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, flake-compat, ... }:
+  inputs.pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, flake-compat, pre-commit-hooks, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -20,6 +22,20 @@
       in
       with pkgs;
       {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              doc = {
+                enable = true;
+                description = "Generate figures";
+                entry = "make doc";
+                types_or = [ "plantuml" ];
+                pass_filenames = false;
+              };
+            };
+          };
+        };
         devShells.default = mkShell
           {
             buildInputs = [
@@ -27,6 +43,7 @@
               plantuml
               coreutils
             ];
+            shellHook = self.checks.${system}.pre-commit-check.shellHook;
           };
       }
     );
