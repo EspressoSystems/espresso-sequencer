c608e607eb4083aa364c1b4e0130fe6d8f1e1be4
sveitser <sveitser@gmail.com>
2022-11-22 15:08:00 +0100

    Add dev env, sequencer and zkevm crates
    - Add precommit hooks for cargo fmt, clippy, cargo sort.
    - Add HotShot dependency and ensure it compiles.
    - Add a bunch of dev tools.
    
    Close #6
    Close #14

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
-  description = "espresso-evm";
+  description = "Espresso Decentralized Sequencer";
+
+  nixConfig = {
+    extra-substituters = [ "https://espresso-systems-private.cachix.org" ];
+    extra-trusted-public-keys = [ "espresso-systems-private.cachix.org-1:LHYk03zKQCeZ4dvg3NctyCq88e44oBZVug5LpYKjPRI=" ];
+  };
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -33,19 +38,71 @@
                 types_or = [ "plantuml" ];
                 pass_filenames = false;
               };
+              cargo-fmt = {
+                enable = true;
+                description = "Enforce rustfmt";
+                entry = "cargo fmt --all";
+                pass_filenames = false;
+              };
+              cargo-sort = {
+                enable = true;
+                description = "Ensure Cargo.toml are sorted";
+                entry = "cargo sort -g -w";
+                pass_filenames = false;
+              };
+              cargo-clippy = {
+                enable = true;
+                description = "Run clippy";
+                entry = "cargo clippy --workspace -- -D clippy::dbg-macro";
+                pass_filenames = false;
+              };
             };
           };
         };
-        devShells.default = mkShell
-          {
-            buildInputs = [
-              entr
-              graphviz
-              plantuml
-              coreutils
-            ];
-            shellHook = self.checks.${system}.pre-commit-check.shellHook;
-          };
+        devShells.default =
+          let
+            stableToolchain = pkgs.rust-bin.stable.latest.minimal.override {
+              extensions = [ "rustfmt" "clippy" "llvm-tools-preview" "rust-src" ];
+            };
+            # nixWithFlakes allows pre v2.4 nix installations to use
+            # flake commands (like `nix flake update`)
+            nixWithFlakes = pkgs.writeShellScriptBin "nix" ''
+              exec ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
+            '';
+          in
+          mkShell
+            {
+              buildInputs = [
+                # Rust dependencies
+                pkgconfig
+                openssl
+                curl
+                protobuf # to compile libp2p-autonat
+                stableToolchain
+
+                # Rust tools
+                cargo-edit
+                cargo-watch
+                cargo-sort
+
+                # Tools
+                nixWithFlakes
+                entr
+
+                # Figures
+                graphviz
+                plantuml
+                coreutils
+              ];
+              shellHook = ''
+                # Prevent cargo aliases from using programs in `~/.cargo` to avoid conflicts
+                # with rustup installations.
+                export CARGO_HOME=$HOME/.cargo-nix
+              '' + self.checks.${system}.pre-commit-check.shellHook;
+              RUST_SRC_PATH = "${stableToolchain}/lib/rustlib/src/rust/library";
+              RUST_BACKTRACE = 1;
+              RUST_LOG = "info,libp2p=off";
+            };
       }
     );
 }
