8609575bb88c92d3998a8b27b7063c723f0d04ca
sveitser <sveitser@gmail.com>
2022-11-23 21:04:38 +0100

    WIP: sequencer skeleton

--- sequencer/src/lib.rs
+++ sequencer/src/lib.rs
@@ -1,14 +1,128 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use commit::{Commitment, Committable};
+use hotshot::{
+    traits::{
+        election::static_committee::{StaticCommittee, StaticElectionConfig, StaticVoteToken},
+        implementations::{CentralizedServerNetwork, MemoryStorage},
+        Block as HotShotBlock, NodeImplementation, State as HotShotState,
+    },
+    types::ed25519::Ed25519Pub,
+};
+use hotshot_types::{
+    data::ViewNumber,
+    traits::{block_contents::Transaction as HotShotTransaction, node_implementation::NodeTypes},
+};
+use serde::{Deserialize, Serialize};
+use snafu::Snafu;
+
+#[derive(Debug, Clone)]
+struct Node;
+
+impl NodeImplementation<SeqTypes> for Node {
+    type Storage = MemoryStorage<SeqTypes>;
+
+    type Networking = CentralizedServerNetwork<SeqTypes>;
+
+    type Election = StaticCommittee; // ???
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(
+    Clone, Copy, Debug, Default, Hash, Eq, PartialEq, PartialOrd, Ord, Deserialize, Serialize,
+)]
+struct SeqTypes;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+type SignatureKey = Ed25519Pub;
+
+impl NodeTypes for SeqTypes {
+    type Time = ViewNumber;
+
+    type BlockType = Block;
+
+    type SignatureKey = SignatureKey;
+
+    type VoteTokenType = StaticVoteToken<SignatureKey>;
+
+    type Transaction = Transaction;
+
+    type ElectionConfigType = StaticElectionConfig;
+
+    type StateType = State;
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize, Hash, PartialEq, Eq)]
+struct Block;
+
+impl HotShotBlock for Block {
+    type Error = Error;
+
+    type Transaction = Transaction;
+
+    fn add_transaction_raw(
+        &self,
+        tx: &Self::Transaction,
+    ) -> std::result::Result<Self, Self::Error> {
+        todo!()
+    }
+
+    fn contained_transactions(&self) -> std::collections::HashSet<Commitment<Self::Transaction>> {
+        todo!()
+    }
+}
+
+impl Committable for Block {
+    fn commit(&self) -> Commitment<Self> {
+        todo!()
+    }
+}
+
+#[derive(Clone, Debug, Snafu, Deserialize, Serialize)]
+pub enum Error {
+    Error,
+}
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
+struct Transaction;
+
+impl HotShotTransaction for Transaction {}
+
+impl Committable for Transaction {
+    fn commit(&self) -> Commitment<Self> {
+        todo!()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Default, Hash, PartialEq, Eq)]
+struct State;
+
+impl HotShotState for State {
+    type Error = Error;
+
+    type BlockType = Block;
+
+    type Time = ViewNumber;
+
+    fn next_block(&self) -> Self::BlockType {
+        todo!()
+    }
+
+    fn validate_block(&self, block: &Self::BlockType, view_number: &Self::Time) -> bool {
+        todo!()
+    }
+
+    fn append(
+        &self,
+        block: &Self::BlockType,
+        view_number: &Self::Time,
+    ) -> Result<Self, Self::Error> {
+        todo!()
+    }
+
+    fn on_commit(&self) {
+        todo!()
+    }
+}
+
+impl Committable for State {
+    fn commit(&self) -> Commitment<Self> {
+        todo!()
     }
 }
