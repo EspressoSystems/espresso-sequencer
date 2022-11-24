b2b7a9d1e6a30c971c06bf892df41c49bbbdeda6
sveitser <sveitser@gmail.com>
2022-11-24 18:17:50 +0100

    Add VMs and Application transactions

--- sequencer/src/lib.rs
+++ sequencer/src/lib.rs
@@ -1,22 +1,52 @@
+use async_trait::async_trait;
 use commit::{Commitment, Committable};
-use hotshot::traits::{
-    election::{
-        static_committee::{StaticCommittee, StaticElectionConfig, StaticVoteToken},
-        vrf::JfPubKey,
+use hotshot::{
+    traits::{
+        election::{
+            static_committee::{StaticCommittee, StaticElectionConfig, StaticVoteToken},
+            vrf::JfPubKey,
+        },
+        implementations::{CentralizedServerNetwork, MemoryStorage},
+        Block as HotShotBlock, NodeImplementation, State as HotShotState,
     },
-    implementations::{CentralizedServerNetwork, MemoryStorage},
-    Block as HotShotBlock, NodeImplementation, State as HotShotState,
+    types::HotShotHandle,
+    HotShotError,
 };
 use hotshot_types::{
     data::ViewNumber,
     traits::{block_contents::Transaction as HotShotTransaction, node_implementation::NodeTypes},
 };
 use jf_primitives::signatures::BLSSignatureScheme;
-use serde::{Deserialize, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use snafu::Snafu;
+use std::fmt::Debug;
 
 #[derive(Debug, Clone)]
 struct Node;
+
+// Name to be decided. Adapted from Espresso Validator.
+#[async_trait]
+trait Api {
+    type Error: std::error::Error + Debug;
+    async fn submit<V: Vm>(&mut self, txn: V::Transaction) -> Result<(), Self::Error>;
+    // async fn next_event(&mut self) -> Result<ConsensusEvent, Self::Error>;
+}
+
+#[async_trait]
+impl<N> Api for HotShotHandle<SeqTypes, N>
+where
+    N: NodeImplementation<SeqTypes>,
+{
+    type Error = HotShotError<SeqTypes>;
+
+    async fn submit<V: Vm>(&mut self, txn: V::Transaction) -> Result<(), Self::Error> {
+        self.submit_transaction(Transaction {
+            vm: V::id(),
+            payload: bincode::serialize(&txn).unwrap(),
+        })
+        .await
+    }
+}
 
 impl NodeImplementation<SeqTypes> for Node {
     type Storage = MemoryStorage<SeqTypes>;
@@ -81,7 +111,18 @@ pub enum Error {
 }
 
 #[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
-struct Transaction;
+struct VmId(u64);
+
+#[derive(Clone, Serialize, Deserialize, Debug, PartialEq, Eq, Hash)]
+struct Transaction {
+    vm: VmId,
+    payload: Vec<u8>,
+}
+
+trait Vm {
+    type Transaction: DeserializeOwned + Serialize + Sync + Send;
+    fn id() -> VmId;
+}
 
 impl HotShotTransaction for Transaction {}
 
