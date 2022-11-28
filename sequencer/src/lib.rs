60aaa5375cfd7924ea005b1156e3a1e70426f878
sveitser <sveitser@gmail.com>
2022-11-28 15:56:35 +0100

    Instantiate HotShot inside a dummy test

--- sequencer/src/lib.rs
+++ sequencer/src/lib.rs
@@ -4,7 +4,7 @@ use hotshot::{
     traits::{
         election::{
             static_committee::{StaticCommittee, StaticElectionConfig, StaticVoteToken},
-            vrf::BlsPubKey,
+            vrf::JfPubKey,
         },
         implementations::{CentralizedServerNetwork, MemoryStorage},
         Block as HotShotBlock, NodeImplementation, State as HotShotState,
@@ -16,6 +16,7 @@ use hotshot_types::{
     data::ViewNumber,
     traits::{block_contents::Transaction as HotShotTransaction, node_implementation::NodeTypes},
 };
+use jf_primitives::signatures::BLSSignatureScheme;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use snafu::Snafu;
 use std::fmt::Debug;
@@ -66,16 +67,18 @@ impl NodeImplementation<SeqTypes> for Node {
 )]
 struct SeqTypes;
 
-type SignatureKey = BlsPubKey;
+type Param381 = ark_bls12_381::Parameters;
+type SignatureSchemeType = BLSSignatureScheme<Param381>;
+type SignatureKeyType = JfPubKey<SignatureSchemeType>;
 
 impl NodeTypes for SeqTypes {
     type Time = ViewNumber;
 
     type BlockType = Block;
 
-    type SignatureKey = SignatureKey;
+    type SignatureKey = SignatureKeyType;
 
-    type VoteTokenType = StaticVoteToken<SignatureKey>;
+    type VoteTokenType = StaticVoteToken<SignatureKeyType>;
 
     type Transaction = Transaction;
 
@@ -174,27 +177,44 @@ impl Committable for State {
     }
 }
 
+#[cfg(test)]
 mod test {
+    use super::*;
+    use hotshot::{HotShot, HotShotInitializer};
+    use hotshot_types::{traits::metrics::NoMetrics, ExecutionType, HotShotConfig};
+    use jf_primitives::signatures::SignatureScheme; // This trait provides the `key_gen` method.
+    use rand::thread_rng;
     use std::{net::SocketAddr, time::Duration};
 
-    use hotshot::HotShot;
-    use hotshot_types::{traits::metrics::NoMetrics, ExecutionType, HotShotConfig};
-    use jf_primitives::signatures::{BLSSignatureScheme, SignatureScheme};
-    use rand::thread_rng;
+    #[ignore] // Currently hangs, probably waiting for centralized server that isn't running.
+    #[async_std::test]
+    async fn test_skeleton_instatiation() -> Result<(), ()> {
+        let num_nodes = 3usize;
 
-    use super::*;
+        // Generate keys for the nodes.
+        let nodes_key_pairs = (0..num_nodes)
+            .map(|_| SignatureSchemeType::key_gen(&(), &mut thread_rng()).unwrap())
+            .collect::<Vec<_>>();
 
-    fn test_skeleton() {
-        type SignatureScheme = BLSSignatureScheme<ark_bls12_381::Parameters>;
-        let (private_key, public_key) = SignatureScheme::key_gen(&(), &mut thread_rng()).unwrap();
+        // Convert public keys to JfPubKey
+        let nodes_pub_keys = nodes_key_pairs
+            .iter()
+            .map(|(_sign_key, ver_key)| JfPubKey::from_native(ver_key.clone()))
+            .collect::<Vec<_>>();
+
+        // Create public and private keys for the first node.
+        let (sign_key, ver_key) = nodes_key_pairs[0].clone();
+        // let private_key = sign_key;
+        let public_key = JfPubKey::from_native(ver_key.clone());
+
         let node_id = 0u64;
-        let total_nodes = 3usize.try_into().unwrap();
-        let config: HotShotConfig<SignatureKey, _> = HotShotConfig {
+
+        let config: HotShotConfig<_, _> = HotShotConfig {
             execution_type: ExecutionType::Incremental,
-            total_nodes,
+            total_nodes: num_nodes.try_into().unwrap(),
             min_transactions: 0,
             max_transactions: 5usize.try_into().unwrap(),
-            known_nodes: vec![],
+            known_nodes: nodes_pub_keys.clone(),
             next_view_timeout: Duration::from_secs(60).as_millis() as u64,
             timeout_ratio: (10, 11),
             round_start_delay: Duration::from_millis(1).as_millis() as u64,
@@ -206,22 +226,33 @@ mod test {
         };
 
         let centralized_server_address: SocketAddr = "127.0.0.1:11223".parse().unwrap();
-        // TODO: start server
-        let networking = CentralizedServerNetwork::<SeqTypes>::connect_with_server_config(
-            NoMetrics::new(),
-            centralized_server_address,
-        );
+        // TODO: run the centralized server
+        let (_network_config, _run, network) =
+            CentralizedServerNetwork::<SeqTypes>::connect_with_server_config(
+                NoMetrics::new(),
+                centralized_server_address,
+            )
+            .await;
+        let storage = MemoryStorage::<SeqTypes>::new();
+        let election = StaticCommittee::<SeqTypes>::new(nodes_pub_keys);
+        let genesis_block = Block {};
+        let initializer = HotShotInitializer::<SeqTypes>::from_genesis(genesis_block).unwrap();
+        let metrics = NoMetrics::new();
 
-        // let hotshot = HotShot::init(
-        //     public_key,
-        //     private_key,
-        //     node_id,
-        //     config,
-        //     networking,
-        //     storage,
-        //     election,
-        //     initializer,
-        //     metrics,
-        // )
+        let _hotshot: HotShotHandle<SeqTypes, Node> = HotShot::init(
+            public_key,
+            (sign_key, ver_key),
+            node_id,
+            config,
+            network,
+            storage,
+            election,
+            initializer,
+            metrics,
+        )
+        .await
+        .unwrap();
+
+        Ok(())
     }
 }
