1439fe56aa59e6cf31a1d9838ab1a2247f98c8c5
sveitser <sveitser@gmail.com>
2023-02-01 17:30:04 +0100

    WIP: run zkevm-node with custom contracts
    Status: Messy WIP, the zkevm-node appears to start up correctly but
    there's currently no test to check if it actually works.

--- contract-bindings/examples/deploy.rs
+++ contract-bindings/examples/deploy.rs
@@ -1,3 +1,9 @@
+use std::{
+    io::{BufRead, BufReader},
+    path::Path,
+    process::Command,
+};
+
 use contract_bindings::{polygon_zk_evm::PolygonZkEVM, shared_types::BatchData, TestHermezSystem};
 use ethers::{providers::Middleware as _, types::BlockNumber};
 
@@ -5,39 +11,115 @@ use ethers::{providers::Middleware as _, types::BlockNumber};
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    // Start L1
+    Command::new("docker")
+        .current_dir(
+            Path::new(env!("CARGO_MANIFEST_DIR"))
+                .parent()
+                .unwrap()
+                .join("zkevm-node/test"),
+        )
+        .arg("compose")
+        .arg("-f")
+        .arg("permissionless-docker-compose.yml")
+        .arg("--env-file")
+        .arg("../../.env")
+        .arg("up")
+        .arg("zkevm-mock-l1-network")
+        .arg("-V")
+        .arg("--force-recreate")
+        .arg("--abort-on-container-exit")
+        .spawn()
+        .expect("Failed to start L1 docker container");
+
+    println!("Waiting for L1 to start ...");
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
     let TestHermezSystem {
-        provider,
+        // provider,
         rollup,
-        clients,
+        // clients,
+        matic,
+        global_exit_root,
         ..
     } = TestHermezSystem::deploy().await;
 
-    // Try to sequence a batch
-    let l2_tx_data = hex::decode("1234").unwrap();
+    // // Try to sequence a batch
+    // let l2_tx_data = hex::decode("1234").unwrap();
 
-    let current_timestamp = provider
-        .get_block(BlockNumber::Latest)
-        .await
-        .unwrap()
-        .unwrap()
-        .timestamp;
+    // let current_timestamp = provider
+    //     .get_block(BlockNumber::Latest)
+    //     .await
+    //     .unwrap()
+    //     .unwrap()
+    //     .timestamp;
 
-    let batch = BatchData {
-        transactions: l2_tx_data.into(),
-        global_exit_root: [0u8; 32],
-        timestamp: current_timestamp.as_u64(),
-        min_forced_timestamp: 0u64,
-    };
+    // let batch = BatchData {
+    //     transactions: l2_tx_data.into(),
+    //     global_exit_root: [0u8; 32],
+    //     timestamp: current_timestamp.as_u64(),
+    //     min_forced_timestamp: 0u64,
+    // };
 
-    // Sent a batch
-    let rollup_trusted: PolygonZkEVM<_> = rollup.connect(clients.trusted_sequencer).into();
-    let receipt = rollup_trusted
-        .sequence_batches(vec![batch])
-        .send()
-        .await
-        .unwrap()
-        .await
-        .unwrap();
+    // // Send a batch
+    // let rollup_trusted: PolygonZkEVM<_> = rollup.connect(clients.trusted_sequencer).into();
+    // let receipt = rollup_trusted
+    //     .sequence_batches(vec![batch])
+    //     .send()
+    //     .await
+    //     .unwrap()
+    //     .await
+    //     .unwrap();
 
-    assert_eq!(receipt.unwrap().status, Some(1u64.into()));
+    // assert_eq!(receipt.unwrap().status, Some(1u64.into()));
+
+    // Start zkevm-node
+    let mut cmd = Command::new("docker");
+
+    cmd.env(
+        "ZKEVM_NODE_ETHERMAN_POEADDR",
+        format!("{:?}", rollup.address()),
+    )
+    .env(
+        "ZKEVM_NODE_ETHERMAN_MATICADDR",
+        format!("{:?}", matic.address()),
+    )
+    .env(
+        "ZKEVM_NODE_ETHERMAN_GLOBALEXITROOTMANAGERADDR",
+        format!("{:?}", global_exit_root.address()),
+    )
+    .current_dir(
+        Path::new(env!("CARGO_MANIFEST_DIR"))
+            .parent()
+            .unwrap()
+            .join("zkevm-node/test"),
+    )
+    .arg("compose")
+    .arg("-f")
+    .arg("permissionless-docker-compose.yml")
+    .arg("--env-file")
+    .arg("../../.env")
+    .arg("up")
+    .arg("-V")
+    // .arg("--force-recreate")
+    .arg("--abort-on-container-exit");
+
+    for (k, v) in cmd.get_envs() {
+        println!("ENV {k:?}: {v:?}");
+    }
+
+    println!("Starting zkevm-node {cmd:?}");
+
+    let mut handle = cmd.spawn().expect("Failed to start docker");
+
+    handle.wait().unwrap();
+
+    // let stdout = cmd.stdout.as_mut().unwrap();
+    // let stdout_reader = BufReader::new(stdout);
+    // let stdout_lines = stdout_reader.lines();
+
+    // for line in stdout_lines {
+    //     println!("Read: {:?}", line);
+    // }
+    println!("Done!")
 }
