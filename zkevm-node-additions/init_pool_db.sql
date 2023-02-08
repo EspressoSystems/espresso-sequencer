ac06cf56041380a56968a41952a14b00cf280e6b
sveitser <sveitser@gmail.com>
2023-02-08 11:03:51 +0100

    Put state and pool DB into same postgres instance
    - Add an SQL init script for the pool DB.
    - Remove 1G memory reservation for postgres.
    - Work around postgres being "ready" too early.
    - Remove zkevm-pool-db container.

---
+++ zkevm-node-additions/init_pool_db.sql
@@ -0,0 +1,4 @@
+-- Create the database for the pool. In the combined database, this DB is not
+-- created via the `POSTGRES_DB` environment variable passed to the docker
+-- container.
+CREATE DATABASE pool_db;
