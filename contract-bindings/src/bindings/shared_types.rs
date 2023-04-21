b5ce9f257bb58aff86ce9204dd7fd77076ec9d7a
Philippe Camacho <philippe@espressosys.com>
2023-04-21 10:29:36 -0400

    Add contract bindings files again.

--- contract-bindings/src/bindings/shared_types.rs
+++ contract-bindings/src/bindings/shared_types.rs
@@ -1,17 +1,3 @@
-#[doc = "`FuzzSelector(address,bytes4[])`"]
-#[derive(
-    Clone,
-    Debug,
-    Default,
-    Eq,
-    PartialEq,
-    ethers :: contract :: EthAbiType,
-    ethers :: contract :: EthAbiCodec,
-)]
-pub struct FuzzSelector {
-    pub addr: ethers::core::types::Address,
-    pub selectors: Vec<[u8; 4]>,
-}
 #[doc = "`PackedHotShotParams(bytes32,bytes32,bytes)`"]
 #[derive(
     Clone,
@@ -26,6 +12,20 @@ pub struct PackedHotShotParams {
     pub old_acc_input_hash: [u8; 32],
     pub new_acc_input_hash: [u8; 32],
     pub comm_proof: ethers::core::types::Bytes,
+}
+#[doc = "`FuzzSelector(address,bytes4[])`"]
+#[derive(
+    Clone,
+    Debug,
+    Default,
+    Eq,
+    PartialEq,
+    ethers :: contract :: EthAbiType,
+    ethers :: contract :: EthAbiCodec,
+)]
+pub struct FuzzSelector {
+    pub addr: ethers::core::types::Address,
+    pub selectors: Vec<[u8; 4]>,
 }
 #[doc = "`InitializePackedParameters(address,address,uint64,address,uint64)`"]
 #[derive(
