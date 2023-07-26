1fb94ba683b2c2420a68296fe8a5a64117ffad32
nomaxg <noahgolub2@gmail.com>
2023-07-26 12:36:41 -0400

    delete script
    Signed-off-by: nomaxg <noahgolub2@gmail.com>

--- sequencer.sh
+++
@@ -1,7 +0,0 @@
-for i in `seq $1`; do
-      target/release/sequencer \
-              --orchestrator-url http://localhost:8080 \
-              --da-server-url http://localhost:8081 \
-              --consensus-server-url http://localhost:8082 &
-      sleep 15
-done
