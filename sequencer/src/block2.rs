e5c008d25713ea9b837cd09fb42decb964ddf2ae
Gus Gutoski <ggutoski@users.noreply.github.com>
2023-11-20 14:41:22 -0500

    index gymnastics in prep for transaction_with_proof()

--- sequencer/src/block2.rs
+++ sequencer/src/block2.rs
@@ -62,12 +62,60 @@ impl BlockPayload {
         Self { payload }
     }
 
-    fn tx_table_len(&self) -> u32 {
-        // TODO don't panic
-        // Alternative: store this value as a separate field of `Self`.
-        u32::from_le_bytes(self.payload[..size_of::<u32>()].try_into().unwrap())
+    // TODO comment
+    fn get_tx_bytes(&self, index: TxIndex) -> Option<&[u8]> {
+        let tx_bodies_offset = self.tx_bodies_offset()?;
+
+        // TODO clean up this comment.
+        // tx_table[i] is end index for the ith tx,
+        // so the range for the ith tx is tx_table[i-1..i].
+        // But tx_table starts at index 1 in the payload
+        // because index 0 in the payload is the length of the tx_table.
+        // So the range for the ith tx in the payload is actually
+        // [index..index+1].
+        let start = if index == 0 {
+            0
+        } else {
+            self.get_value_usize(index)?
+        }
+        .checked_add(tx_bodies_offset)?;
+        let end = self
+            .get_value_usize(index.checked_add(1)?)?
+            .checked_add(tx_bodies_offset)?;
+
+        self.payload.get(start..end)
+    }
+
+    // TODO comment
+    // View self.payload as a bunch of TxIndex, return the indexth one.
+    fn get_value(&self, index: TxIndex) -> Option<TxIndex> {
+        // TODO idiomatic usize <-> TxIndex conversion.
+        let start = usize::try_from(index)
+            .unwrap()
+            .checked_mul(size_of::<TxIndex>())?;
+        let end = start.checked_add(size_of::<TxIndex>())?;
+        Some(TxIndex::from_le_bytes(
+            self.payload.get(start..end)?.try_into().unwrap(),
+        ))
+    }
+    fn get_value_usize(&self, index: TxIndex) -> Option<usize> {
+        Some(usize::try_from(self.get_value(index)?).unwrap())
+    }
+
+    fn tx_table_len(&self) -> Option<TxIndex> {
+        self.get_value(0)
+    }
+    fn tx_table_len_usize(&self) -> Option<usize> {
+        self.get_value_usize(0)
+    }
+    fn tx_bodies_offset(&self) -> Option<usize> {
+        self.tx_table_len_usize()?
+            .checked_add(1)?
+            .checked_mul(size_of::<TxIndex>())
     }
 }
+
+type TxIndex = <BlockPayload as QueryableBlock>::TransactionIndex;
 
 impl QueryableBlock for BlockPayload {
     type TransactionIndex = u32;
@@ -75,22 +123,19 @@ impl QueryableBlock for BlockPayload {
     type InclusionProof = ();
 
     fn len(&self) -> usize {
-        // TODO don't panic
-        // Alternative: do `as usize` and pray?
-        // We should probably fail the build if usize is smaller than u32.
-        self.tx_table_len()
-            .try_into()
-            .expect("number of txs should fit into usize")
+        self.tx_table_len_usize().unwrap_or(0)
     }
 
     fn iter(&self) -> Self::Iter<'_> {
-        0..self.tx_table_len()
+        0..self.tx_table_len().unwrap_or(0)
     }
 
     fn transaction_with_proof(
         &self,
         _index: &Self::TransactionIndex,
     ) -> Option<(&Self::Transaction, Self::InclusionProof)> {
+        // let tx_bytes = self.get_tx_bytes(index)?;
+        // jf_primitives::vid::advz::
         todo!()
     }
 }
@@ -140,7 +185,7 @@ mod boilerplate {
 
 #[cfg(test)]
 mod test {
-    use super::BlockPayload;
+    use super::{BlockPayload, QueryableBlock, TxIndex};
     use crate::Transaction;
     use std::mem::size_of;
 
@@ -176,13 +221,13 @@ mod test {
             vec![],
         ];
 
-        for tx_payloads in test_cases {
+        for tx_bodies in test_cases {
             // prepare things as a function of the test case
-            let txs = tx_payloads
+            let txs = tx_bodies
                 .iter()
                 .cloned()
                 .map(|payload| Transaction::new(crate::VmId(0), payload));
-            let tx_offsets: Vec<u32> = tx_payloads
+            let tx_offsets: Vec<u32> = tx_bodies
                 .iter()
                 .scan(0, |end, tx| {
                     *end += u32::try_from(tx.len()).unwrap();
@@ -195,10 +240,10 @@ mod test {
             // test tx table length
             let (tx_table_len_bytes, payload) = block.payload.split_at(size_of::<u32>());
             let tx_table_len = u32::from_le_bytes(tx_table_len_bytes.try_into().unwrap());
-            assert_eq!(tx_table_len, u32::try_from(tx_payloads.len()).unwrap());
+            assert_eq!(tx_table_len, u32::try_from(tx_bodies.len()).unwrap());
 
             // test tx table contents
-            let (tx_table_bytes, payload) = payload.split_at(tx_payloads.len() * size_of::<u32>());
+            let (tx_table_bytes, payload) = payload.split_at(tx_bodies.len() * size_of::<u32>());
             let tx_table: Vec<u32> = tx_table_bytes
                 .chunks(size_of::<u32>())
                 .map(|len_bytes| u32::from_le_bytes(len_bytes.try_into().unwrap()))
@@ -206,8 +251,19 @@ mod test {
             assert_eq!(tx_table, tx_offsets);
 
             // test block payload body
-            let tx_payloads_flat: Vec<u8> = tx_payloads.into_iter().flatten().collect();
+            let tx_payloads_flat: Vec<u8> = tx_bodies.iter().flatten().cloned().collect();
             assert_eq!(payload, tx_payloads_flat);
+
+            // test get_tx_bytes()
+            assert_eq!(tx_bodies.len(), block.len());
+            for (index, tx_body) in tx_bodies.iter().enumerate() {
+                assert_eq!(
+                    tx_body,
+                    block
+                        .get_tx_bytes(TxIndex::try_from(index).unwrap())
+                        .unwrap()
+                )
+            }
         }
     }
 }
