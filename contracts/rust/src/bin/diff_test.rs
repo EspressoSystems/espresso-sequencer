b0ba4b6222fc9c02e9c65111c9663ebff4400d09
Philippe Camacho <philippe@espressosys.com>
2023-11-27 11:29:00 -0300

    Add TODO for returning ParsedG1(G2)Point.

--- contracts/rust/src/bin/diff_test.rs
+++ contracts/rust/src/bin/diff_test.rs
@@ -429,6 +429,8 @@ fn main() {
             let sig_x = field_to_u256::<Fq>(sig_affine_point.x);
             let sig_y = field_to_u256::<Fq>(sig_affine_point.y);
 
+            // TODO (Alex) add a TODO:
+            // Return ParsedG1Point and ParsedG2Point in https://github.com/EspressoSystems/espresso-sequencer/issues/615 instead of field by field
             let res = (
                 sig_x,
                 sig_y,
