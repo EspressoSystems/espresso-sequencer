ac5a281ca29e1b1d79afbf206a0ae8643033ef89
Philippe Camacho <philippe@espressosys.com>
2023-11-27 11:36:58 -0300

    Simplify the computation of .

--- contracts/rust/src/bin/diff_test.rs
+++ contracts/rust/src/bin/diff_test.rs
@@ -401,8 +401,7 @@ fn main() {
                 panic!("Should provide arg1=senderAddress");
             }
 
-            let sender_address_string = cli.args[0].clone();
-            let sender_address: Address = AbiDecode::decode_hex(sender_address_string).unwrap();
+            let sender_address: Address = AbiDecode::decode_hex(&cli.args[0]).unwrap();
             let sender_address_bytes = AbiEncode::encode(sender_address);
 
             // Generate the Schnorr key
