a1dd04daf38800dab742569b0d3a999e0ddba867
Mathis Antony <sveitser@gmail.com>
2023-11-29 12:18:34 +0100

    Update bindings (on OSX)
    Currently on my Linux desktop I don't get any changes but on OSX the
    metadata hash (I believe) changes.

--- contract-bindings/src/safe_transfer_lib.rs
+++ contract-bindings/src/safe_transfer_lib.rs
@@ -25,12 +25,12 @@ pub mod safe_transfer_lib {
     pub static SAFETRANSFERLIB_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> =
         ::ethers::contract::Lazy::new(__abi);
     #[rustfmt::skip]
-    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 4\xA9\xD5\xF4\x14xB\xD9\xF2\xB3T,^\x0E\xC7\x96\xAD\xBB\xAC\x9B\xBB\x95\xEA,\r\xA2L\xE2\xCA\x18`4dsolcC\0\x08\x14\x003";
+    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xD0\x1B\xD8m\x95\x8C\xB1\xC9\x9C\xE1\x9E\x1E\xF4\xB6\xECZ8\x1B]\xD7\x9DG_\xD1\xB3\xA5\x14z\xCA\xF3.\x88dsolcC\0\x08\x14\x003";
     /// The bytecode of the contract.
     pub static SAFETRANSFERLIB_BYTECODE: ::ethers::core::types::Bytes =
         ::ethers::core::types::Bytes::from_static(__BYTECODE);
     #[rustfmt::skip]
-    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 4\xA9\xD5\xF4\x14xB\xD9\xF2\xB3T,^\x0E\xC7\x96\xAD\xBB\xAC\x9B\xBB\x95\xEA,\r\xA2L\xE2\xCA\x18`4dsolcC\0\x08\x14\x003";
+    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xD0\x1B\xD8m\x95\x8C\xB1\xC9\x9C\xE1\x9E\x1E\xF4\xB6\xECZ8\x1B]\xD7\x9DG_\xD1\xB3\xA5\x14z\xCA\xF3.\x88dsolcC\0\x08\x14\x003";
     /// The deployed bytecode of the contract.
     pub static SAFETRANSFERLIB_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes =
         ::ethers::core::types::Bytes::from_static(__DEPLOYED_BYTECODE);
--- contract-bindings/src/utils.rs
+++ contract-bindings/src/utils.rs
@@ -25,12 +25,12 @@ pub mod utils {
     pub static UTILS_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> =
         ::ethers::contract::Lazy::new(__abi);
     #[rustfmt::skip]
-    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x8C3\xF5E\xDDz\x8Ff\x9D\xCC\xA9\x90\x90\xFB*\t~\roAr\xB6\x14\x95N\xB4\xC1P\xD1\xEE\xE6\xCAdsolcC\0\x08\x14\x003";
+    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 a\xF7\xFB\x1E\x94\xC3\x96\x98\xD9+\xE60\xFC\x0E\xE3\x9A\xE5\xB5>{\x8C6\xC2Q\xD8t|Ev\xC1\xB7\xD3dsolcC\0\x08\x14\x003";
     /// The bytecode of the contract.
     pub static UTILS_BYTECODE: ::ethers::core::types::Bytes =
         ::ethers::core::types::Bytes::from_static(__BYTECODE);
     #[rustfmt::skip]
-    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x8C3\xF5E\xDDz\x8Ff\x9D\xCC\xA9\x90\x90\xFB*\t~\roAr\xB6\x14\x95N\xB4\xC1P\xD1\xEE\xE6\xCAdsolcC\0\x08\x14\x003";
+    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 a\xF7\xFB\x1E\x94\xC3\x96\x98\xD9+\xE60\xFC\x0E\xE3\x9A\xE5\xB5>{\x8C6\xC2Q\xD8t|Ev\xC1\xB7\xD3dsolcC\0\x08\x14\x003";
     /// The deployed bytecode of the contract.
     pub static UTILS_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes =
         ::ethers::core::types::Bytes::from_static(__DEPLOYED_BYTECODE);
