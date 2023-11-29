f3ed9e6c160862cafc99142b022e249de5996aef
sveitser <sveitser@gmail.com>
2023-11-29 11:28:59 +0100

    Update bindings

--- contract-bindings/src/bls_sig.rs
+++ contract-bindings/src/bls_sig.rs
@@ -31,12 +31,12 @@ pub mod bls_sig {
     pub static BLSSIG_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> =
         ::ethers::contract::Lazy::new(__abi);
     #[rustfmt::skip]
-    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xDEa\\\xBC\xE9L\xA2\xD0\xF4w\xCD\xA6\xF5\xB9\xA80f;L\xE2\x18\x92\xCD\xD4\x03=\xA91T\xDF&\x10dsolcC\0\x08\x14\x003";
+    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x87\x8Ewm\x03\x19E\xAA\xE7\xBF#\xB3\x1E~\\#!\xEBl\x14K\xB4\xE1\x0FSX!\x1Ec\x97rVdsolcC\0\x08\x17\x003";
     /// The bytecode of the contract.
     pub static BLSSIG_BYTECODE: ::ethers::core::types::Bytes =
         ::ethers::core::types::Bytes::from_static(__BYTECODE);
     #[rustfmt::skip]
-    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \xDEa\\\xBC\xE9L\xA2\xD0\xF4w\xCD\xA6\xF5\xB9\xA80f;L\xE2\x18\x92\xCD\xD4\x03=\xA91T\xDF&\x10dsolcC\0\x08\x14\x003";
+    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x87\x8Ewm\x03\x19E\xAA\xE7\xBF#\xB3\x1E~\\#!\xEBl\x14K\xB4\xE1\x0FSX!\x1Ec\x97rVdsolcC\0\x08\x17\x003";
     /// The deployed bytecode of the contract.
     pub static BLSSIG_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes =
         ::ethers::core::types::Bytes::from_static(__DEPLOYED_BYTECODE);
--- contract-bindings/src/utils.rs
+++ contract-bindings/src/utils.rs
@@ -25,12 +25,12 @@ pub mod utils {
     pub static UTILS_ABI: ::ethers::contract::Lazy<::ethers::core::abi::Abi> =
         ::ethers::contract::Lazy::new(__abi);
     #[rustfmt::skip]
-    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x8C3\xF5E\xDDz\x8Ff\x9D\xCC\xA9\x90\x90\xFB*\t~\roAr\xB6\x14\x95N\xB4\xC1P\xD1\xEE\xE6\xCAdsolcC\0\x08\x14\x003";
+    const __BYTECODE: &[u8] = b"`V`7`\x0B\x82\x82\x829\x80Q`\0\x1A`s\x14`*WcNH{q`\xE0\x1B`\0R`\0`\x04R`$`\0\xFD[0`\0R`s\x81S\x82\x81\xF3\xFEs\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x92\xD5~&\xA60\xBBJ<\xEF\xC7\x99\x88\x9E\x8D*v\xFD\x93\xF8\xE8\x9F\x97\xE4\x1B\xEBi\xB6\x8D\x16]\xEBdsolcC\0\x08\x17\x003";
     /// The bytecode of the contract.
     pub static UTILS_BYTECODE: ::ethers::core::types::Bytes =
         ::ethers::core::types::Bytes::from_static(__BYTECODE);
     #[rustfmt::skip]
-    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x8C3\xF5E\xDDz\x8Ff\x9D\xCC\xA9\x90\x90\xFB*\t~\roAr\xB6\x14\x95N\xB4\xC1P\xD1\xEE\xE6\xCAdsolcC\0\x08\x14\x003";
+    const __DEPLOYED_BYTECODE: &[u8] = b"s\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x000\x14`\x80`@R`\0\x80\xFD\xFE\xA2dipfsX\"\x12 \x92\xD5~&\xA60\xBBJ<\xEF\xC7\x99\x88\x9E\x8D*v\xFD\x93\xF8\xE8\x9F\x97\xE4\x1B\xEBi\xB6\x8D\x16]\xEBdsolcC\0\x08\x17\x003";
     /// The deployed bytecode of the contract.
     pub static UTILS_DEPLOYED_BYTECODE: ::ethers::core::types::Bytes =
         ::ethers::core::types::Bytes::from_static(__DEPLOYED_BYTECODE);
