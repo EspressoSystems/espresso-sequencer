ce1ebbfc4aebae7b41c5ec4abf9acef2f7432bfa
Philippe Camacho <philippe@espressosys.com>
2023-11-29 12:46:40 -0300

    Reorder the tests.

--- contracts/test/StakeTable.t.sol
+++ contracts/test/StakeTable.t.sol
@@ -78,71 +78,6 @@ contract StakeTable_register_Test is Test {
         stakeTable = new S(address(token),lightClientAddress);
     }
 
-    /// @dev Tests a correct registration
-    function test_Registration_succeeds() external {
-        (
-            BN254.G2Point memory blsVK,
-            EdOnBN254.EdOnBN254Point memory schnorrVK,
-            BN254.G1Point memory sig
-        ) = genClientWallet(exampleTokenCreator);
-
-        uint64 depositAmount = 10;
-        uint64 validUntilEpoch = 5;
-
-        // Prepare for the token transfer
-        vm.prank(exampleTokenCreator);
-        token.approve(address(stakeTable), depositAmount);
-
-        // Balances before registration
-        assertEq(token.balanceOf(exampleTokenCreator), INITIAL_BALANCE);
-
-        uint256 nativeAmount;
-        uint256 restakedAmount;
-        (nativeAmount, restakedAmount) = stakeTable.totalStake();
-        assertEq(nativeAmount, 0);
-        assertEq(restakedAmount, 0);
-
-        IStakeTable.Node memory node;
-        node.account = exampleTokenCreator;
-        node.balance = depositAmount;
-        node.stakeType = IStakeTable.StakeType.Native;
-        node.schnorrVK = schnorrVK;
-        node.registerEpoch = 1;
-
-        // Check event is emitted after calling successfully `register`
-        vm.expectEmit(false, false, false, true, address(stakeTable));
-        emit Registered(
-            stakeTable._hashBlsKey(blsVK), node.registerEpoch, node.stakeType, node.balance
-        );
-        vm.prank(exampleTokenCreator);
-        bool res = stakeTable.register(
-            blsVK, schnorrVK, depositAmount, IStakeTable.StakeType.Native, sig, validUntilEpoch
-        );
-
-        assertTrue(res);
-
-        // Balance after registration
-        assertEq(token.balanceOf(exampleTokenCreator), INITIAL_BALANCE - depositAmount);
-        (nativeAmount, restakedAmount) = stakeTable.totalStake();
-        assertEq(nativeAmount, depositAmount);
-        assertEq(restakedAmount, 0);
-    }
-
-    function test_RevertWhen_InvalidBLSSig() external {
-        uint64 depositAmount = 10;
-        uint64 validUntilEpoch = 5;
-
-        (BN254.G2Point memory blsVK, EdOnBN254.EdOnBN254Point memory schnorrVK,) =
-            genClientWallet(msg.sender);
-
-        // Failed signature verification
-        BN254.G1Point memory badSig = BN254.P1();
-        vm.expectRevert(BLSSig.BLSSigVerificationFailed.selector);
-        stakeTable.register(
-            blsVK, schnorrVK, depositAmount, IStakeTable.StakeType.Native, badSig, validUntilEpoch
-        );
-    }
-
     function test_RevertWhen_UsingRestakeToken() external {
         uint64 depositAmount = 10;
         uint64 validUntilEpoch = 5;
@@ -157,6 +92,21 @@ contract StakeTable_register_Test is Test {
         vm.expectRevert(S.RestakingNotImplemented.selector);
         stakeTable.register(
             blsVK, schnorrVK, depositAmount, IStakeTable.StakeType.Restake, sig, validUntilEpoch
+        );
+    }
+
+    function test_RevertWhen_InvalidBLSSig() external {
+        uint64 depositAmount = 10;
+        uint64 validUntilEpoch = 5;
+
+        (BN254.G2Point memory blsVK, EdOnBN254.EdOnBN254Point memory schnorrVK,) =
+            genClientWallet(msg.sender);
+
+        // Failed signature verification
+        BN254.G1Point memory badSig = BN254.P1();
+        vm.expectRevert(BLSSig.BLSSigVerificationFailed.selector);
+        stakeTable.register(
+            blsVK, schnorrVK, depositAmount, IStakeTable.StakeType.Native, badSig, validUntilEpoch
         );
     }
 
@@ -218,5 +168,55 @@ contract StakeTable_register_Test is Test {
         stakeTable.register(
             blsVK, schnorrVK, depositAmount, IStakeTable.StakeType.Native, sig, validUntilEpoch
         );
+    }
+
+    /// @dev Tests a correct registration
+    function test_Registration_succeeds() external {
+        (
+            BN254.G2Point memory blsVK,
+            EdOnBN254.EdOnBN254Point memory schnorrVK,
+            BN254.G1Point memory sig
+        ) = genClientWallet(exampleTokenCreator);
+
+        uint64 depositAmount = 10;
+        uint64 validUntilEpoch = 5;
+
+        // Prepare for the token transfer
+        vm.prank(exampleTokenCreator);
+        token.approve(address(stakeTable), depositAmount);
+
+        // Balances before registration
+        assertEq(token.balanceOf(exampleTokenCreator), INITIAL_BALANCE);
+
+        uint256 nativeAmount;
+        uint256 restakedAmount;
+        (nativeAmount, restakedAmount) = stakeTable.totalStake();
+        assertEq(nativeAmount, 0);
+        assertEq(restakedAmount, 0);
+
+        IStakeTable.Node memory node;
+        node.account = exampleTokenCreator;
+        node.balance = depositAmount;
+        node.stakeType = IStakeTable.StakeType.Native;
+        node.schnorrVK = schnorrVK;
+        node.registerEpoch = 1;
+
+        // Check event is emitted after calling successfully `register`
+        vm.expectEmit(false, false, false, true, address(stakeTable));
+        emit Registered(
+            stakeTable._hashBlsKey(blsVK), node.registerEpoch, node.stakeType, node.balance
+        );
+        vm.prank(exampleTokenCreator);
+        bool res = stakeTable.register(
+            blsVK, schnorrVK, depositAmount, IStakeTable.StakeType.Native, sig, validUntilEpoch
+        );
+
+        assertTrue(res);
+
+        // Balance after registration
+        assertEq(token.balanceOf(exampleTokenCreator), INITIAL_BALANCE - depositAmount);
+        (nativeAmount, restakedAmount) = stakeTable.totalStake();
+        assertEq(nativeAmount, depositAmount);
+        assertEq(restakedAmount, 0);
     }
 }
