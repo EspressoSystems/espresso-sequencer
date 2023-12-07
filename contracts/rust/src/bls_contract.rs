a1822e87043ea2668b6849c93608c9c421ebfe2f
Philippe Camacho <philippe@espressosys.com>
2023-12-07 13:50:52 -0300

    Revert "Remove files related to old HotShot contract and BLS signature testing."
    This reverts commit 41242b82cbf174b5ead9a4a916cad9f22741fd54.

---
+++ contracts/rust/src/bls_contract.rs
@@ -0,0 +1,126 @@
+#[cfg(test)]
+
+mod test {
+
+    use crate::helpers::hotshot_contract::TestBLSSystem;
+    use crate::helpers::{compare_field_elems, compare_group_elems, MyG1Point, MyG2Point};
+    use ark_bn254::Fq;
+    use ark_ec::CurveGroup;
+    use ark_ff::field_hashers::{DefaultFieldHasher, HashToField};
+    use ark_std::vec;
+    use async_compatibility_layer::logging::{setup_backtrace, setup_logging};
+    use ethers::types::Bytes;
+    use jf_primitives::signatures::bls_over_bn254::{
+        hash_to_curve, BLSOverBN254CurveSignatureScheme,
+    };
+    use jf_primitives::signatures::SignatureScheme;
+    use jf_utils::test_rng;
+    use sequencer_utils::AnvilOptions;
+    use sha3::Keccak256;
+
+    fn test_inputs() -> Vec<Bytes> {
+        let message1 = Bytes::from(vec![1u8, 2u8, 3u8, 45u8, 88u8]);
+        let mut message2 = vec![1u8, 2u8, 3u8, 45u8, 88u8];
+        let csid = b"BLS_SIG_BN254G1_XMD:KECCAK_NCTH_NUL_";
+        message2.extend(csid);
+        let message2 = Bytes::from(message2);
+
+        let message3 = Bytes::from(vec![33u8; 1000]);
+
+        vec![message1, message2, message3]
+    }
+
+    #[async_std::test]
+    #[ignore] // TODO Philippe use forge instead
+    async fn test_full_sig_scheme() {
+        setup_logging();
+        setup_backtrace();
+
+        let rng = &mut test_rng();
+        let message = Bytes::from(vec![1u8, 2u8, 3u8, 45u8, 88u8]);
+
+        // TODO why can't we write let parameters = (); ? cargo clippy complains
+        let (sk, pk) = BLSOverBN254CurveSignatureScheme::key_gen(&(), rng).unwrap();
+        let sig = BLSOverBN254CurveSignatureScheme::sign(&(), &sk, &message, rng).unwrap();
+        assert!(BLSOverBN254CurveSignatureScheme::verify(&(), &pk, &message, &sig).is_ok());
+
+        let anvil = AnvilOptions::default().spawn().await;
+        let provider = anvil.provider();
+        let TestBLSSystem { bls, .. } = TestBLSSystem::deploy(provider).await.unwrap();
+
+        let sig_value: MyG1Point = sig.clone().sigma.into_affine().into();
+
+        let pk_value: MyG2Point = pk.to_affine().into();
+
+        let res = bls
+            .verify_bls_sig(
+                message.clone(),
+                sig_value.clone().into(),
+                pk_value.clone().into(),
+            )
+            .call()
+            .await;
+
+        assert!(res.is_ok());
+
+        let wrong_message = Bytes::from(vec![10u8; 3]);
+        assert!(
+            BLSOverBN254CurveSignatureScheme::verify(&(), &pk, wrong_message.clone(), &sig)
+                .is_err()
+        );
+        let res = bls
+            .verify_bls_sig(
+                wrong_message.clone(),
+                sig_value.clone().into(),
+                pk_value.clone().into(),
+            )
+            .call()
+            .await;
+
+        assert!(res.is_err());
+
+        let (_, wrong_pk) = BLSOverBN254CurveSignatureScheme::key_gen(&(), rng).unwrap();
+        assert!(BLSOverBN254CurveSignatureScheme::verify(&(), &wrong_pk, &message, &sig).is_err());
+
+        let wrong_pk_value: MyG2Point = wrong_pk.to_affine().into();
+        let res = bls
+            .verify_bls_sig(message.clone(), sig_value.into(), wrong_pk_value.into())
+            .call()
+            .await;
+        assert!(res.is_err());
+    }
+
+    #[async_std::test]
+    async fn test_hash_to_field() {
+        let anvil = AnvilOptions::default().spawn().await;
+        let provider = anvil.provider();
+        let TestBLSSystem { bls, .. } = TestBLSSystem::deploy(provider).await.unwrap();
+
+        // Same as in the hash_to_curve function
+        // See https://github.com/EspressoSystems/jellyfish/blob/6c2c08f4e966fd1d454d48bcf30bd41a952f9f76/primitives/src/signatures/bls_over_bn254.rs#L310
+        let hasher_init = &[1u8];
+        let hasher = <DefaultFieldHasher<Keccak256> as HashToField<Fq>>::new(hasher_init);
+
+        let msgs = test_inputs();
+
+        for msg in msgs.iter() {
+            let x_rust: Fq = hasher.hash_to_field(msg, 1)[0];
+            let x_contract = bls.hash_to_field(msg.clone()).call().await.unwrap();
+            compare_field_elems(x_rust, x_contract);
+        }
+    }
+
+    #[async_std::test]
+    async fn test_hash_to_curve() {
+        let anvil = AnvilOptions::default().spawn().await;
+        let provider = anvil.provider();
+        let TestBLSSystem { bls, .. } = TestBLSSystem::deploy(provider).await.unwrap();
+
+        let msgs = test_inputs();
+        for msg in msgs.iter() {
+            let group_elem = hash_to_curve::<Keccak256>(msg);
+            let group_elem_contract = bls.hash_to_curve(msg.clone()).call().await.unwrap();
+            compare_group_elems(group_elem.into(), group_elem_contract);
+        }
+    }
+}
---
+++ contracts/rust/src/hotshot_contract.rs
@@ -0,0 +1,266 @@
+#[cfg(test)]
+
+mod test {
+    use crate::helpers::{MyG1Point, MyG2Point};
+    use ark_ec::CurveGroup;
+    use contract_bindings::hot_shot::{HotShot, NewBlocksCall, Qc};
+    use contract_bindings::shared_types::G2Point;
+    use ethers::middleware::SignerMiddleware;
+    use ethers::signers::Wallet;
+    use ethers::types::Bytes;
+    use ethers::{abi::AbiDecode, providers::Middleware, types::U256};
+    use ethers_providers::Http;
+    use jf_primitives::signatures::bls_over_bn254::{
+        BLSOverBN254CurveSignatureScheme, SignKey, Signature, VerKey,
+    };
+    use jf_primitives::signatures::{AggregateableSignatureSchemes, SignatureScheme};
+    use jf_utils::test_rng;
+    use sequencer_utils::test_utils::TestL1System;
+    use sequencer_utils::AnvilOptions;
+
+    enum ResultExpected {
+        Ok,
+        Error,
+    }
+
+    #[async_std::test]
+    async fn test_hotshot_block_commitment() {
+        let anvil = AnvilOptions::default().spawn().await;
+        let provider = anvil.provider();
+        let TestL1System { hotshot, .. } = TestL1System::deploy(provider.clone()).await.unwrap();
+
+        let block_num = U256::from(0);
+        let commitment = U256::from(1234);
+        let qcs = vec![Qc {
+            height: block_num,
+            block_commitment: commitment,
+            ..Default::default()
+        }];
+
+        hotshot
+            .new_blocks(qcs.clone())
+            .send()
+            .await
+            .unwrap()
+            .await
+            .unwrap();
+
+        assert_eq!(
+            hotshot.commitments(block_num).call().await.unwrap(),
+            commitment,
+        );
+
+        let (event, meta) = &hotshot
+            .new_blocks_filter()
+            .from_block(0)
+            // Ethers does not set the contract address on filters created via contract bindings.
+            // This seems like a bug and I have reported it:
+            // https://github.com/gakonst/ethers-rs/issues/2528. In the mean time we can work around
+            // by setting the address manually.
+            .address(hotshot.address().into())
+            .query_with_meta()
+            .await
+            .unwrap()[0];
+
+        assert_eq!(event.first_block_number, block_num);
+        assert_eq!(event.num_blocks, 1.into());
+
+        // Parse the commitments from calldata.
+        let tx = provider
+            .get_transaction(meta.transaction_hash)
+            .await
+            .unwrap()
+            .unwrap();
+        let call = NewBlocksCall::decode(&tx.input).unwrap();
+        assert_eq!(call.qcs, qcs);
+    }
+
+    #[async_std::test]
+    async fn test_hotshot_stake_table() {
+        let anvil = AnvilOptions::default().spawn().await;
+        let provider = anvil.provider();
+        let TestL1System { hotshot, .. } = TestL1System::deploy(provider.clone()).await.unwrap();
+
+        let rng = &mut test_rng();
+        for i in 0..5 {
+            let (_, pk) = BLSOverBN254CurveSignatureScheme::key_gen(&(), rng).unwrap();
+            let pk = pk.to_affine();
+            let pk_value: MyG2Point = pk.into();
+            let amount = U256::from(10 * i);
+            hotshot
+                .add_new_staking_key(pk_value.clone().into(), amount)
+                .send()
+                .await
+                .unwrap()
+                .await
+                .unwrap();
+
+            let expected_stake_and_amount = (G2Point::from(pk_value), amount);
+            assert_eq!(
+                hotshot.get_staking_key(U256::from(i)).call().await.unwrap(),
+                expected_stake_and_amount,
+            );
+        }
+    }
+
+    fn get_slice_from_bitmap<T: Clone>(input: Vec<T>, bitmap: &[bool]) -> Vec<T> {
+        let mut res = vec![];
+        for i in 0..input.len() {
+            if bitmap[i] {
+                res.push(input[i].clone());
+            }
+        }
+        res
+    }
+
+    async fn check_agg_sig(
+        bitmap: &Vec<bool>,
+        staking_keys: &[(SignKey, VerKey)],
+        messages: &[Bytes],
+        stake_threshold: U256,
+        hotshot: &HotShot<
+            SignerMiddleware<
+                ethers::providers::Provider<Http>,
+                Wallet<ethers::core::k256::ecdsa::SigningKey>,
+            >,
+        >,
+        res_expected: ResultExpected,
+    ) {
+        let n_sigs = bitmap.len();
+        let rng = &mut test_rng();
+        let mut signatures: Vec<Signature> = vec![];
+
+        for (key_pair, message) in staking_keys.iter().take(n_sigs).zip(messages) {
+            let sk = key_pair.0.clone();
+            let sig = BLSOverBN254CurveSignatureScheme::sign(&(), &sk, message, rng).unwrap();
+            signatures.push(sig.clone());
+        }
+
+        let vks = get_slice_from_bitmap(
+            staking_keys.iter().map(|k| k.1).collect::<Vec<VerKey>>(),
+            bitmap,
+        );
+        let sigs = get_slice_from_bitmap(signatures, bitmap);
+        let agg_sig = BLSOverBN254CurveSignatureScheme::aggregate(&(), &vks, &sigs).unwrap();
+        let agg_sig_value: MyG1Point = agg_sig.clone().sigma.into_affine().into();
+
+        // Call the contract
+
+        let message = messages[0].clone();
+
+        let res = hotshot
+            .verify_agg_sig(
+                message.clone(),
+                agg_sig_value.into(),
+                bitmap.to_vec(),
+                stake_threshold,
+            )
+            .call()
+            .await;
+
+        match res_expected {
+            ResultExpected::Ok => assert!(res.is_ok()),
+            ResultExpected::Error => assert!(res.is_err()),
+        }
+    }
+
+    #[async_std::test]
+    #[ignore] // TODO Philippe This contract will be removed
+    async fn test_validate_qc() {
+        let anvil = AnvilOptions::default().spawn().await;
+        let provider = anvil.provider();
+        let TestL1System { hotshot, .. } = TestL1System::deploy(provider.clone()).await.unwrap();
+
+        // Initialize the staking table with 5 keys
+        let mut staking_keys = vec![];
+        let rng = &mut test_rng();
+        let n_sigs = 5;
+        for i in 0..n_sigs {
+            let (sk, pk) = BLSOverBN254CurveSignatureScheme::key_gen(&(), rng).unwrap();
+            staking_keys.push((sk.clone(), pk));
+            let pk = pk.to_affine();
+            let pk_value: MyG2Point = pk.into();
+            let amount = U256::from(10 * (i + 1));
+            hotshot
+                .add_new_staking_key(pk_value.clone().into(), amount)
+                .send()
+                .await
+                .unwrap()
+                .await
+                .unwrap();
+        }
+
+        // Happy path
+
+        // Each signer is expected to sign the same message
+        let messages = vec![Bytes::from(b"unique message"); n_sigs];
+        let stake_threshold = U256::from(10);
+        check_agg_sig(
+            &vec![true, true, true, false, false],
+            &staking_keys,
+            &messages,
+            stake_threshold,
+            &hotshot,
+            ResultExpected::Ok,
+        )
+        .await;
+
+        check_agg_sig(
+            &vec![false, true, true, false, true],
+            &staking_keys,
+            &messages,
+            stake_threshold,
+            &hotshot,
+            ResultExpected::Ok,
+        )
+        .await;
+
+        check_agg_sig(
+            &vec![true, false, false, false, false],
+            &staking_keys,
+            &messages,
+            stake_threshold,
+            &hotshot,
+            ResultExpected::Ok,
+        )
+        .await;
+
+        // Error cases
+
+        // Threshold is too low
+        check_agg_sig(
+            &vec![true, false, false, false, false],
+            &staking_keys,
+            &messages,
+            U256::from(1000),
+            &hotshot,
+            ResultExpected::Error,
+        )
+        .await;
+
+        // Signature is invalid because one message differs
+        let unique_message = Bytes::from(b"unique message");
+        let mut messages = vec![unique_message; n_sigs];
+        messages[0] = Bytes::from(b"different message");
+        check_agg_sig(
+            &vec![true, true, true, false, true],
+            &staking_keys,
+            &messages,
+            stake_threshold,
+            &hotshot,
+            ResultExpected::Error,
+        )
+        .await;
+
+        // Bitmap length is too high
+        check_agg_sig(
+            &vec![true; 32],
+            &staking_keys,
+            &messages,
+            stake_threshold,
+            &hotshot,
+            ResultExpected::Error,
+        )
+        .await;
+    }
+}
