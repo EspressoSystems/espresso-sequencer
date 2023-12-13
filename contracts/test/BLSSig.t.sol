b756e829314362cdf5566ab80741865158c62340
Philippe Camacho <philippe@espressosys.com>
2023-12-13 06:34:17 -0300

    Run *foge fmt*

--- contracts/test/BLSSig.t.sol
+++ contracts/test/BLSSig.t.sol
@@ -30,7 +30,6 @@ contract BLSSig_Test is Test {
     }
 
     function testFuzz_BLS_hashes_computation(bytes memory input) external {
-
         string[] memory cmds = new string[](3);
         cmds[0] = "diff-test";
         cmds[1] = "gen-bls-hashes";
