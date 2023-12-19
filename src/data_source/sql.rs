47c0355f669ed1147ede9921d8bb4a5f82dff61c
Jeb Bearer <jeb@espressosys.com>
2023-12-19 09:03:26 -0500

    Update src/data_source/sql.rs
    Co-authored-by: Mathis <sveitser@gmail.com>

--- src/data_source/sql.rs
+++ src/data_source/sql.rs
@@ -336,7 +336,7 @@ impl Config {
 ///
 /// ## Resetting
 ///
-/// In general, resetting the database when necessary is left up to the adminstrator. However, for
+/// In general, resetting the database when necessary is left up to the administrator. However, for
 /// convenience, we do provide a [`reset_schema`](Config::reset_schema) option which can be used to
 /// wipe out existing state and create a fresh instance of the query service. This is particularly
 /// useful for development and staging environments. This function will permanently delete all
