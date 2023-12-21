8c955a227c9fbee0aab6b2d209947d6c93696632
Jeb Bearer <jeb@espressosys.com>
2023-12-21 17:32:20 -0500

    Make persistence options independent of query service
    This commit restructures the configuration of persistence so it is
    now a separate optional module from the query service. The user can
    choose between SQL and file system persistence, as before, and it
    will be used to persist data which is required for the node to run.
    Currently, this is just the network config file from the orchestrator.
    If no persistence is chosen, it defaults to the local file system.
    
    There is now only a single command to start the query service, no
    more `query-fs` and `query-sql`. If enabled it will use whatever
    persistence was previously chosen.
    
    Interface changes:
    * Sequencer command line requires `query`, not `query-fs` or `query-sql`
    * Sequencer command line requires `storage-sql` if you want to use SQL
    * There is no longer an option to run the commitment task with the sequencer
      (this was tech debt anyway, and it was easier to remove it than fix it)
    * `ESPRESSO_SEQUENCER_CONFIG_PATH` is gone. The config file will be
      persisted in whatever persistence is chosen (`ESPRESSO_SEQUENCER_CONFIG_PATH`
      if using the file system)
    
    Closes #804
    Closes #886

--- sequencer/src/api/sql.rs
+++ sequencer/src/api/sql.rs
@@ -1,7 +1,5 @@
-use super::{
-    data_source::SequencerDataSource, endpoints::TimeWindowQueryData, options::Sql as Options,
-};
-use crate::{Header, SeqTypes};
+use super::{data_source::SequencerDataSource, endpoints::TimeWindowQueryData};
+use crate::{persistence::sql::Options, Header, SeqTypes};
 use async_trait::async_trait;
 use futures::{StreamExt, TryStreamExt};
 use hotshot_query_service::{
@@ -17,7 +15,7 @@ pub type DataSource = SqlDataSource<SeqTypes>;
 impl SequencerDataSource for DataSource {
     type Options = Options;
 
-    async fn create(opt: Self::Options) -> anyhow::Result<Self> {
+    async fn create(opt: Self::Options, reset: bool) -> anyhow::Result<Self> {
         let mut cfg =
             Config::default().migrations(include_migrations!("$CARGO_MANIFEST_DIR/api/migrations"));
 
@@ -27,16 +25,19 @@ impl SequencerDataSource for DataSource {
         if let Some(port) = opt.port {
             cfg = cfg.port(port);
         }
-        if let Some(database) = opt.database {
+        if let Some(database) = &opt.database {
             cfg = cfg.database(database);
         }
-        if let Some(user) = opt.user {
+        if let Some(user) = &opt.user {
             cfg = cfg.user(user);
         }
-        if let Some(password) = opt.password {
+        if let Some(password) = &opt.password {
             cfg = cfg.password(password);
         }
-        if opt.reset_store {
+        if opt.use_tls {
+            cfg = cfg.tls();
+        }
+        if reset {
             cfg = cfg.reset_schema();
         }
 
@@ -200,12 +201,15 @@ mod impl_testable_data_source {
         }
 
         fn options(storage: &Self::Storage, opt: api::Options) -> api::Options {
-            opt.query_sql(Options {
-                port: Some(storage.port()),
-                user: Some("postgres".into()),
-                password: Some("password".into()),
-                ..Default::default()
-            })
+            opt.query_sql(
+                Default::default(),
+                Options {
+                    port: Some(storage.port()),
+                    user: Some("postgres".into()),
+                    password: Some("password".into()),
+                    ..Default::default()
+                },
+            )
         }
     }
 }
