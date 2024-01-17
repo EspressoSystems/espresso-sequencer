74eaa22f305c2d8101cce30c30ac069aaab8c919
Alysia Huggins <hi@alysia.tech>
2024-01-17 10:48:42 -0500

    added additional tests and comments

--- contracts/src/FeeContract.sol
+++ contracts/src/FeeContract.sol
@@ -29,17 +29,20 @@ contract FeeContract {
     /// @notice store user balances in a mapping
     mapping(address user => uint256 amount) public balances;
 
+    /// @notice Revert if a method name does not exist
     fallback() external payable {
         // send / transfer (forwards 2300 gas to this fallback function)
         // call (forwards all of the gas)
         revert FunctionDoesNotExist();
     }
 
-    // Receive is a variant of fallback that is triggered when msg.data is empty
+    /// @notice Revert if no method name was called
     receive() external payable {
         revert NoFunctionCalled();
     }
 
+    /// @notice Allows anyone to deposit an ETH balance for any user
+    /// @dev the deposit amount is less than a specified threshold to prevent fat finger errors
     function deposit(address user) public payable {
         if (msg.value == 0) {
             revert NonZeroDepositAmount();
@@ -54,6 +57,7 @@ contract FeeContract {
         emit Deposit(user, msg.value);
     }
 
+    /// @notice Allows anyone to get the balance an ETH balance for any user
     function getBalance(address user) public view returns (uint256 amount) {
         if (user == address(0)) {
             revert InvalidUserAddress();
