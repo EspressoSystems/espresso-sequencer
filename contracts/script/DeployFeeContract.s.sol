fd5bdd1b09d29146c1df649d7a64d100ba5e783f
Alysia Huggins <hi@alysia.tech>
2024-02-01 09:41:23 -0500

    refactor fee contract deploy script

--- contracts/script/DeployFeeContract.s.sol
+++ contracts/script/DeployFeeContract.s.sol
@@ -7,22 +7,17 @@ import { FeeContract } from "../src/FeeContract.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract DeployFeeContract is Script {
-    function run() external returns (address payable proxy, address admin) {
-        // string memory seedPhrase = vm.envString("MNEMONIC");
-        // (address admin,) = deriveRememberKey(seedPhrase, 0);
-        (proxy, admin) = deployFeeContract();
-
-        return (proxy, admin);
-    }
-
     /// @notice deploys the impl, proxy & initializes the impl
     /// @return proxyAddress The address of the proxy
     /// @return admin The address of the admin
-    function deployFeeContract() public returns (address payable proxyAddress, address admin) {
+    function run() external returns (address payable proxyAddress, address admin) {
+        // string memory seedPhrase = vm.envString("MNEMONIC");
+        // (address admin,) = deriveRememberKey(seedPhrase, 0);
         vm.startBroadcast();
 
-        FeeContract feeContract = new FeeContract(); //Our implementation(logic).Proxy will point
-            // here to delegate
+        //Our implementation(logic).Proxy will point
+        // here to delegate
+        FeeContract feeContract = new FeeContract();
 
         // Encode the initializer function call
         bytes memory data = abi.encodeWithSelector(
@@ -30,11 +25,14 @@ contract DeployFeeContract is Script {
             msg.sender // Initial owner/admin of the contract
         );
 
+        // our proxy
         ERC1967Proxy proxy = new ERC1967Proxy(address(feeContract), data);
         vm.stopBroadcast();
 
+        //get the owner
         proxyAddress = payable(address(proxy));
         admin = FeeContract(proxyAddress).owner();
+
         return (proxyAddress, admin);
     }
 }
