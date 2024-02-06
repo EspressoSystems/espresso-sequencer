bebdae99f4d59eaa1298eaaf821216a9490cbc82
Alysia Huggins <hi@alysia.tech>
2024-02-06 06:01:16 -0400

    update gitignore to remove broadcast logs

--- contracts/src/FeeContract.sol
+++ contracts/src/FeeContract.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
-// import { console } from "forge-std/console.sol";
 
 import { OwnableUpgradeable } from
     "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
