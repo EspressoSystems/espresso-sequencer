6cbb024def82c1c51088789393d4eaa519e2046b
Chengyu Lin <linmrain@gmail.com>
2024-02-13 20:03:49 -0500

    Update hotshot-state-prover/src/snark.rs
    Co-authored-by: Philippe Camacho <philippe@espressosys.com>

--- hotshot-state-prover/src/snark.rs
+++ hotshot-state-prover/src/snark.rs
@@ -48,7 +48,7 @@ pub fn preprocess<const STAKE_TABLE_CAPACITY: usize>(
 /// Returns error or a pair `(proof, public_inputs)` asserting that
 /// - the signer's accumulated weight exceeds the quorum threshold
 /// - the stake table corresponds to the one committed in the light client state
-/// - all signed schnorr signatures are valid
+/// - all schnorr signatures over the light client state are valid
 /// # Errors
 /// Errors if unable to generate proof
 /// # Panics
