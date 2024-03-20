c2e98bdac59ddea4d46f1583c92f93ba4be81321
Jeb Bearer <jeb@espressosys.com>
2024-03-20 17:56:40 -0400

    Fix Docker demo
    * Fix typo in deploy image path
    * Make deploy script runnable outside of the source tree

--- sequencer/src/bin/deploy.rs
+++ sequencer/src/bin/deploy.rs
@@ -21,9 +21,9 @@ use serde_json::Value;
 use std::{
     collections::HashMap,
     fs::File,
-    io::{stdout, BufReader, Write},
+    io::{stdout, Write},
     ops::Deref,
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
 use url::Url;
 
@@ -295,11 +295,15 @@ async fn deploy_light_client_contract<M: Middleware + 'static>(
         )
         .await?;
 
-    // Link with LightClient's bytecode artifacts
-    let bytecode_path = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("../contracts/out/LightClient.sol/LightClient.json");
-    let json: Value = serde_json::from_reader(BufReader::new(File::open(bytecode_path)?))?;
-
+    // Link with LightClient's bytecode artifacts. We include the unlinked bytecode for the contract
+    // in this binary so that the contract artifacts do not have to be distributed with the binary.
+    // This should be fine because if the bindings we are importing are up to date, so should be the
+    // contract artifacts: this is no different than foundry inlining bytecode objects in generated
+    // bindings, except that foundry doesn't provide the bytecode for contracts that link with
+    // libraries, so we have to do it ourselves.
+    let json: Value = serde_json::from_str(include_str!(
+        "../../../contracts/out/LightClient.sol/LightClient.json"
+    ))?;
     let mut bytecode =
         serde_json::from_value::<BytecodeObject>(json["bytecode"]["object"].clone())?;
     bytecode
