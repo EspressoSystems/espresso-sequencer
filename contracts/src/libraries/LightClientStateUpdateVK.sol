057548efdba7685b9c593f839390b7c7679895df
Philippe Camacho <philippe@espressosys.com>
2024-03-25 18:14:26 -0300

    Fix tests.

--- contracts/src/libraries/LightClientStateUpdateVK.sol
+++ contracts/src/libraries/LightClientStateUpdateVK.sol
@@ -20,6 +20,8 @@ import { IPlonkVerifier } from "../interfaces/IPlonkVerifier.sol";
 /* solhint-disable no-inline-assembly */
 
 library LightClientStateUpdateVK {
+    uint256 constant LIGHTCLIENT_VK_ID = 123;
+
     function getVk() internal pure returns (IPlonkVerifier.VerifyingKey memory vk) {
         assembly {
             // domain size
@@ -192,6 +194,8 @@ library LightClientStateUpdateVK {
                 add(mload(add(vk, 0x260)), 0x20),
                 20309550876545766116130682111350015544103338784776768395329281357767924326613
             )
+            // ID
+            mstore(mload(add(vk, 0x280)), LIGHTCLIENT_VK_ID)
         }
     }
 }
