4e6300046c58650c9c88c7790fd6679c81b6b7cd
Philippe Camacho <philippe@espressosys.com>
2024-03-27 10:41:26 -0300

    Document how to deploy the light client contract and send an update tx on Sepolia

--- contracts/script/LightClient.s.sol
+++ contracts/script/LightClient.s.sol
@@ -5,6 +5,7 @@ import "forge-std/Script.sol";
 
 import { LightClient as LC } from "../src/LightClient.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { LightClientTest as LCTest } from "../test/mocks/LightClientTest.sol";
 
 contract DeployLightClientContractScript is Script {
     function run(uint32 numBlocksPerEpoch, uint32 numInitValidators)
@@ -23,7 +24,26 @@ contract DeployLightClientContractScript is Script {
         (LC.LightClientState memory state,,) =
             abi.decode(result, (LC.LightClientState, bytes32, bytes32));
 
-        return deployContract(state, numBlocksPerEpoch);
+        return deployContract(state, numBlocksPerEpoch, false);
+    }
+
+    function runTest(uint32 numBlocksPerEpoch, uint32 numInitValidators)
+        external
+        returns (address payable proxyAddress, address admin, LC.LightClientState memory)
+    {
+        // TODO for a production deployment provide the right genesis state and value
+
+        string[] memory cmds = new string[](4);
+        cmds[0] = "diff-test";
+        cmds[1] = "mock-genesis";
+        cmds[2] = vm.toString(numBlocksPerEpoch);
+        cmds[3] = vm.toString(uint256(numInitValidators));
+
+        bytes memory result = vm.ffi(cmds);
+        (LC.LightClientState memory state,,) =
+            abi.decode(result, (LC.LightClientState, bytes32, bytes32));
+
+        return deployContract(state, numBlocksPerEpoch, true);
     }
 
     function runDemo(uint32 numBlocksPerEpoch)
@@ -36,36 +56,42 @@ contract DeployLightClientContractScript is Script {
         bytes memory result = vm.ffi(cmds);
         LC.LightClientState memory state = abi.decode(result, (LC.LightClientState));
 
-        return deployContract(state, numBlocksPerEpoch);
+        return deployContract(state, numBlocksPerEpoch, false);
     }
 
     /// @notice deploys the impl, proxy & initializes the impl
     /// @return proxyAddress The address of the proxy
     /// @return admin The address of the admin
 
-    function deployContract(LC.LightClientState memory state, uint32 numBlocksPerEpoch)
+    function deployContract(LC.LightClientState memory state, uint32 numBlocksPerEpoch, bool isTest)
         private
         returns (address payable proxyAddress, address admin, LC.LightClientState memory)
     {
         string memory seedPhrase = vm.envString("MNEMONIC");
         (admin,) = deriveRememberKey(seedPhrase, 0);
         vm.startBroadcast(admin);
+        LC lightClientContract;
+        if (isTest) {
+            lightClientContract = new LCTest(state, numBlocksPerEpoch);
+            vm.stopBroadcast();
+            return (payable(address(lightClientContract)), admin, state);
+        } else {
+            lightClientContract = new LC();
 
-        LC lightClientContract = new LC();
+            // Encode the initializer function call
+            bytes memory data = abi.encodeWithSignature(
+                "initialize((uint64,uint64,uint256,uint256,uint256,uint256,uint256,uint256),uint32)",
+                state,
+                numBlocksPerEpoch
+            );
 
-        // Encode the initializer function call
-        bytes memory data = abi.encodeWithSignature(
-            "initialize((uint64,uint64,uint256,uint256,uint256,uint256,uint256,uint256),uint32)",
-            state,
-            numBlocksPerEpoch
-        );
+            // our proxy
+            ERC1967Proxy proxy = new ERC1967Proxy(address(lightClientContract), data);
+            vm.stopBroadcast();
 
-        // our proxy
-        ERC1967Proxy proxy = new ERC1967Proxy(address(lightClientContract), data);
-        vm.stopBroadcast();
+            proxyAddress = payable(address(proxy));
 
-        proxyAddress = payable(address(proxy));
-
-        return (proxyAddress, admin, state);
+            return (proxyAddress, admin, state);
+        }
     }
 }
