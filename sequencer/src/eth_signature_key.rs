4aac7291d865edeb8bd69afc6d2f628faf65ca56
Mathis Antony <sveitser@gmail.com>
2024-04-15 19:06:10 +0800

    [ci skip] Add a test

--- sequencer/src/eth_signature_key.rs
+++ sequencer/src/eth_signature_key.rs
@@ -9,7 +9,7 @@ use std::{
     hash::Hash,
 };
 
-// Newtype because type doesn't implement Hash, Display
+// Newtype because type doesn't implement Hash, Display, SerDe, Ord, PartialOrd
 #[derive(Debug, From, PartialEq, Eq, Clone)]
 pub struct EthSigKey(SigningKey);
 
@@ -50,12 +50,12 @@ impl Ord for EthSigKey {
 }
 
 #[derive(Clone, Debug, Snafu)]
-pub struct SignError;
+pub struct SigningError;
 
 impl BuilderSignatureKey for EthSigKey {
     type BuilderPrivateKey = Self;
     type BuilderSignature = Signature;
-    type SignError = SignError;
+    type SignError = SigningError;
 
     fn validate_builder_signature(&self, signature: &Self::BuilderSignature, data: &[u8]) -> bool {
         let verifying_key = VerifyingKey::from(&self.0);
@@ -69,7 +69,7 @@ impl BuilderSignatureKey for EthSigKey {
     ) -> Result<Self::BuilderSignature, Self::SignError> {
         let wallet = LocalWallet::from_bytes(&private_key.0.to_bytes()).unwrap();
         let message_hash = ethers::utils::hash_message(data);
-        wallet.sign_hash(message_hash).map_err(|_| SignError)
+        wallet.sign_hash(message_hash).map_err(|_| SigningError)
     }
 
     fn generated_from_seed_indexed(seed: [u8; 32], index: u64) -> (Self, Self::BuilderPrivateKey) {
@@ -79,5 +79,21 @@ impl BuilderSignatureKey for EthSigKey {
         let new_seed = *hasher.finalize().as_bytes();
         let signing_key = EthSigKey::from(SigningKey::from_slice(&new_seed).unwrap());
         (signing_key.clone(), signing_key)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hotshot_types::traits::signature_key::BuilderSignatureKey;
+
+    #[test]
+    fn test_signing_and_recovery() {
+        let seed = [0u8; 32];
+        let index = 0;
+        let (signing_key, _) = EthSigKey::generated_from_seed_indexed(seed, index);
+        let message = b"hello world";
+        let signature = EthSigKey::sign_builder_message(&signing_key, message).unwrap();
+        assert!(signing_key.validate_builder_signature(&signature, message));
     }
 }
