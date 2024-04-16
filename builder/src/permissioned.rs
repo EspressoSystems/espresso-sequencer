2e9da0e25f433705d1fbe87305b1ca105d759c9f
Rob <rob@espressosys.com>
2024-04-16 15:15:49 -0400

    feature flag libp2p

--- builder/src/permissioned.rs
+++ builder/src/permissioned.rs
@@ -137,7 +137,7 @@ pub async fn init_node<P: SequencerPersistence, Ver: StaticVersionType + 'static
     channel_capacity: NonZeroUsize,
     bind_version: Ver,
     persistence: P,
-) -> anyhow::Result<BuilderContext<network::Combined, P, Ver>> {
+) -> anyhow::Result<BuilderContext<network::Networks, P, Ver>> {
     // Orchestrator client
     let validator_args = ValidatorArgs {
         url: network_params.orchestrator_url,
@@ -190,7 +190,8 @@ pub async fn init_node<P: SequencerPersistence, Ver: StaticVersionType + 'static
     .await
     .with_context(|| "Failed to create CDN network")?;
 
-    // Initialize the Libp2p network
+    // Initialize the Libp2p network (if enabled)
+    #[cfg(feature = "libp2p")]
     let p2p_network = Libp2pNetwork::from_config::<SeqTypes>(
         config.clone(),
         network_params.libp2p_bind_address,
@@ -202,17 +203,25 @@ pub async fn init_node<P: SequencerPersistence, Ver: StaticVersionType + 'static
     .await
     .with_context(|| "Failed to create libp2p network")?;
 
-    // Combine the two communication channels
-    let da_network = Arc::from(CombinedNetworks::new(
-        cdn_network.clone(),
-        p2p_network.clone(),
-        Duration::from_secs(1),
-    ));
-    let quorum_network = Arc::from(CombinedNetworks::new(
-        cdn_network,
-        p2p_network,
-        Duration::from_secs(1),
-    ));
+    // Combine the communication channels
+    #[cfg(feature = "libp2p")]
+    let (da_network, quorum_network) = {
+        (
+            Arc::from(CombinedNetworks::new(
+                cdn_network.clone(),
+                p2p_network.clone(),
+                Duration::from_secs(1),
+            )),
+            Arc::from(CombinedNetworks::new(
+                cdn_network,
+                p2p_network,
+                Duration::from_secs(1),
+            )),
+        )
+    };
+
+    #[cfg(not(feature = "libp2p"))]
+    let (da_network, quorum_network) = { (Arc::from(cdn_network.clone()), Arc::from(cdn_network)) };
 
     // Convert to the sequencer-compatible type
     let networks = Networks {
