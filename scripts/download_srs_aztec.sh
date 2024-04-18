863420689173df477ec9b8ef99a90db66a029445
Alex Xiong <alex.xiong.tech@gmail.com>
2024-04-18 21:52:49 +0800

    another attempt to pacify nix

--- scripts/download_srs_aztec.sh
+++ scripts/download_srs_aztec.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+export AZTEC_SRS_PATH="${AZTEC_SRS_PATH:-$PWD/data/aztec20/kzg10-aztec20-srs-1048584.bin}"
+
 if [ -f "$AZTEC_SRS_PATH" ]; then
     echo "SRS file $AZTEC_SRS_PATH exists"
 else
