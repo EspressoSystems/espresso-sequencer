43171a8128f446a132d792a4be8345b2c8bab7c4
Philippe Camacho <philippe@espressosys.com>
2024-04-26 19:53:06 -0400

    Simplification of expression for .

--- hotshot-state-prover/src/mock_ledger.rs
+++ hotshot-state-prover/src/mock_ledger.rs
@@ -479,7 +479,7 @@ pub fn gen_plonk_proof_for_test(
         .zip(prove_keys.iter())
         .enumerate()
         .for_each(|(_, (cs, pk))| {
-            let extra_msg = Some(String::new().into_bytes()); // We set extra_msg="" for the contract tests to pass
+            let extra_msg = Some(vec![]); // We set extra_msg="" for the contract tests to pass
             proofs.push(
                 PlonkKzgSnark::<Bn254>::prove::<_, _, SolidityTranscript>(
                     rng,
