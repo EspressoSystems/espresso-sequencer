99bf8bbc8fc252078b5b4d3226c9ee5312a700fa
Gus Gutoski <ggutoski@users.noreply.github.com>
2024-05-02 15:04:14 -0400

    move module ns_iter to its own file

--- sequencer/src/block2/ns_table.rs
+++ sequencer/src/block2/ns_table.rs
@@ -1,13 +1,12 @@
 use super::payload_bytes::{
-    ns_id_from_bytes, ns_offset_from_bytes, num_nss_as_bytes, num_nss_from_bytes, NS_ID_BYTE_LEN,
-    NS_OFFSET_BYTE_LEN, NUM_NSS_BYTE_LEN,
+    num_nss_from_bytes, NS_ID_BYTE_LEN, NS_OFFSET_BYTE_LEN, NUM_NSS_BYTE_LEN,
 };
 use crate::NamespaceId;
 use ns_iter::{NsIndex, NsIter};
 use serde::{Deserialize, Serialize};
-use std::collections::HashSet;
 
 // TODO do these all need to be pub?
+pub mod ns_iter;
 pub mod ns_payload;
 pub mod ns_payload_range;
 
@@ -57,87 +56,5 @@ impl NsTable {
         // to consume an iterator. If performance is a concern then we could
         // cache this count on construction of `Payload`.
         self.iter().count()
-    }
-}
-
-// TODO move this module to a separate file?
-// TODO pub(crate) only for iter module
-pub(crate) mod ns_iter {
-    use super::*; // TODO temp
-
-    #[derive(Clone, Debug, Deserialize, Eq, Hash, PartialEq, Serialize)]
-    pub struct NsIndex([u8; NUM_NSS_BYTE_LEN]);
-
-    impl NsTable {
-        /// Read the namespace id from the `index`th entry from the namespace table.
-        ///
-        /// Panics if `index >= self.num_nss()`.
-        pub fn read_ns_id(&self, index: &NsIndex) -> NamespaceId {
-            let start = num_nss_from_bytes(&index.0) * (NS_ID_BYTE_LEN + NS_OFFSET_BYTE_LEN)
-                + NUM_NSS_BYTE_LEN;
-            ns_id_from_bytes(&self.0[start..start + NS_ID_BYTE_LEN])
-        }
-
-        /// Read the namespace offset from the `index`th entry from the namespace table.
-        ///
-        /// Panics if `index >= self.num_nss()`.
-        pub fn read_ns_offset(&self, index: &NsIndex) -> usize {
-            let start = num_nss_from_bytes(&index.0) * (NS_ID_BYTE_LEN + NS_OFFSET_BYTE_LEN)
-                + NUM_NSS_BYTE_LEN
-                + NS_ID_BYTE_LEN;
-            ns_offset_from_bytes(&self.0[start..start + NS_OFFSET_BYTE_LEN])
-        }
-
-        /// Read the namespace offset from the `(index-1)`th entry from the
-        /// namespace table. Returns `None` if `index` is zero.
-        ///
-        /// Panics if `index >= self.num_nss()`.
-        pub fn read_ns_offset_prev(&self, index: &NsIndex) -> Option<usize> {
-            if index.0 == [0; NUM_NSS_BYTE_LEN] {
-                None
-            } else {
-                let prev_index = NsIndex(num_nss_as_bytes(num_nss_from_bytes(&index.0) - 1));
-                Some(self.read_ns_offset(&prev_index))
-            }
-        }
-    }
-
-    /// Return type for [`Payload::ns_iter`].
-    pub struct NsIter<'a> {
-        cur_index: usize,
-        repeat_nss: HashSet<NamespaceId>,
-        num_nss: usize,
-        ns_table: &'a NsTable,
-    }
-
-    impl<'a> NsIter<'a> {
-        pub fn new(ns_table: &'a NsTable) -> Self {
-            Self {
-                cur_index: 0,
-                repeat_nss: HashSet::new(),
-                num_nss: ns_table.num_nss(), // cache for speed
-                ns_table,
-            }
-        }
-    }
-
-    impl<'a> Iterator for NsIter<'a> {
-        type Item = NsIndex;
-
-        fn next(&mut self) -> Option<Self::Item> {
-            while self.cur_index < self.num_nss {
-                let result = NsIndex(num_nss_as_bytes(self.cur_index));
-                let ns_id = self.ns_table.read_ns_id(&result);
-                self.cur_index += 1;
-
-                // skip duplicate namespace IDs
-                if !self.repeat_nss.insert(ns_id) {
-                    continue;
-                }
-
-                return Some(result);
-            }
-            None
-        }
     }
 }
