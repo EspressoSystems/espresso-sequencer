71e9cc56ab82ad0710e6cb2c73fa5a37873704d8
imabdulbasit <baronbasit@gmail.com>
2024-05-13 01:32:05 +0500

    make generated columns not nullable

--- sequencer/api/migrations/V16__merkle_root_columns.sql
+++ sequencer/api/migrations/V16__merkle_root_columns.sql
@@ -1,10 +1,10 @@
 ALTER TABLE header
 ADD column block_merkle_tree_root text
-GENERATED ALWAYS AS (data->>'block_merkle_tree_root') STORED;
+GENERATED ALWAYS AS (data->>'block_merkle_tree_root') STORED NOT NULL;
 
 ALTER TABLE header
 ADD column fee_merkle_tree_root text
-GENERATED ALWAYS as (data->>'fee_merkle_tree_root') STORED;
+GENERATED ALWAYS as (data->>'fee_merkle_tree_root') STORED NOT NULL;
 
 CREATE INDEX header_block_merkle_tree_root_idx ON header (block_merkle_tree_root);
 CREATE INDEX header_fee_merkle_tree_root_idx ON header (fee_merkle_tree_root);
