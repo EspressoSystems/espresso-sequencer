437d476a5e217a9945155b32728bb734a023b2fe
Jeb Bearer <jeb@espressosys.com>
2024-05-20 15:31:27 -0400

    Add serialization compatibility test for Hotshot messages

--- sequencer/src/lib.rs
+++ sequencer/src/lib.rs
@@ -7,8 +7,10 @@ pub mod eth_signature_key;
 mod header;
 pub mod hotshot_commitment;
 pub mod options;
-mod reference_tests;
 pub mod state_signature;
+
+mod message_compat_tests;
+mod reference_tests;
 
 use anyhow::Context;
 use async_std::sync::RwLock;
