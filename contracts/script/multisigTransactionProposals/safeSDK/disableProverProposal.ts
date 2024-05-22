9a83d3848a8ce67221cb9086ce0790b40034da63
sveitser <sveitser@gmail.com>
2024-05-22 15:26:03 +0200

    Run prettier on typescript files

--- contracts/script/multisigTransactionProposals/safeSDK/disableProverProposal.ts
+++ contracts/script/multisigTransactionProposals/safeSDK/disableProverProposal.ts
@@ -1,92 +1,91 @@
-import { ethers } from 'ethers'; 
-import { EthersAdapter } from '@safe-global/protocol-kit'; 
-import SafeApiKit from '@safe-global/api-kit'; 
-import Safe from '@safe-global/protocol-kit'; 
-import { getEnvVar, createSafeTransactionData, isValidEthereumAddress } from './utils'; 
+import { ethers } from "ethers";
+import { EthersAdapter } from "@safe-global/protocol-kit";
+import SafeApiKit from "@safe-global/api-kit";
+import Safe from "@safe-global/protocol-kit";
+import { getEnvVar, createSafeTransactionData, isValidEthereumAddress } from "./utils";
 
 async function main() {
-    try {
-        /**TODO
-         * change from SEPOLIA_RPC_URL to production URL when deploying to production
-         */
-        // Initialize web3 provider using the RPC URL from environment variables
-        const web3Provider = new ethers.JsonRpcProvider(getEnvVar("SEPOLIA_RPC_URL"));
-        // Create a signer using the orchestrator's private key and the web3 provider
-        const orchestratorSigner = new ethers.Wallet(getEnvVar("SAFE_ORCHESTRATOR_PRIVATE_KEY"), web3Provider);
+  try {
+    /**TODO
+     * change from SEPOLIA_RPC_URL to production URL when deploying to production
+     */
+    // Initialize web3 provider using the RPC URL from environment variables
+    const web3Provider = new ethers.JsonRpcProvider(getEnvVar("SEPOLIA_RPC_URL"));
+    // Create a signer using the orchestrator's private key and the web3 provider
+    const orchestratorSigner = new ethers.Wallet(getEnvVar("SAFE_ORCHESTRATOR_PRIVATE_KEY"), web3Provider);
 
-        // Set up Eth Adapter with ethers and the signer
-        const ethAdapter = new EthersAdapter({
-            ethers,
-            signerOrProvider: orchestratorSigner
-        });
+    // Set up Eth Adapter with ethers and the signer
+    const ethAdapter = new EthersAdapter({
+      ethers,
+      signerOrProvider: orchestratorSigner,
+    });
 
-        const chainId = await ethAdapter.getChainId();
-        const safeService = new SafeApiKit({ chainId });
+    const chainId = await ethAdapter.getChainId();
+    const safeService = new SafeApiKit({ chainId });
 
+    const safeAddress = getEnvVar("SAFE_MULTISIG_ADDRESS");
+    isValidEthereumAddress(safeAddress);
+    const safeSdk = await Safe.create({ ethAdapter, safeAddress });
 
-        const safeAddress = getEnvVar("SAFE_MULTISIG_ADDRESS");
-        isValidEthereumAddress(safeAddress);
-        const safeSdk = await Safe.create({ ethAdapter, safeAddress });
+    await proposeDisableProverTransaction(safeSdk, safeService, await orchestratorSigner.getAddress(), safeAddress);
 
-        await proposeDisableProverTransaction(safeSdk, safeService, await orchestratorSigner.getAddress(), safeAddress);
-        
-        console.log(`The other owners of the Safe Multisig wallet need to sign the transaction via the Safe UI https://app.safe.global/transactions/queue?safe=sep:${safeAddress}`);
-    } catch (error) {
-        throw new Error("An error occurred: " + error);
-    }
+    console.log(
+      `The other owners of the Safe Multisig wallet need to sign the transaction via the Safe UI https://app.safe.global/transactions/queue?safe=sep:${safeAddress}`,
+    );
+  } catch (error) {
+    throw new Error("An error occurred: " + error);
+  }
 }
 
 /**
  * Function to propose the transaction data for disabling permissioned prover mode
  * @param {string} safeSDK - An instance of the Safe SDK
- * @param {string} safeService - An instance of the Safe Service 
+ * @param {string} safeService - An instance of the Safe Service
  * @param {string} signerAddress - The address of the address signing the transaction
  * @param {string} safeAddress - The address of the Safe multisig wallet
  */
 export async function proposeDisableProverTransaction(
-    safeSDK: Safe, 
-    safeService:SafeApiKit, 
-    signerAddress: string, 
-    safeAddress: string,
-){
-    // Prepare the transaction data to disable permissioned prover mode
-    let data = createDisablePermissionedProverTxData();
+  safeSDK: Safe,
+  safeService: SafeApiKit,
+  signerAddress: string,
+  safeAddress: string,
+) {
+  // Prepare the transaction data to disable permissioned prover mode
+  let data = createDisablePermissionedProverTxData();
 
-    const contractAddress = getEnvVar("LIGHT_CLIENT_CONTRACT_ADDRESS");
-    isValidEthereumAddress(contractAddress);
-    
-    // Create the Safe Transaction Object
-    const safeTransaction = await createSafeTransaction(safeSDK, contractAddress, data, '0');
+  const contractAddress = getEnvVar("LIGHT_CLIENT_CONTRACT_ADDRESS");
+  isValidEthereumAddress(contractAddress);
 
-    // Get the transaction hash and sign the transaction
-     const safeTxHash = await safeSDK.getTransactionHash(safeTransaction);
+  // Create the Safe Transaction Object
+  const safeTransaction = await createSafeTransaction(safeSDK, contractAddress, data, "0");
 
-    // Sign the transaction with orchestrator signer that was specified when we created the safeSDK
-     const senderSignature = await safeSDK.signHash(safeTxHash);
+  // Get the transaction hash and sign the transaction
+  const safeTxHash = await safeSDK.getTransactionHash(safeTransaction);
 
-     // Propose the transaction which can be signed by other owners via the Safe UI
-     await safeService.proposeTransaction({
-         safeAddress: safeAddress,
-         safeTransactionData: safeTransaction.data,
-         safeTxHash: safeTxHash,
-         senderAddress: signerAddress,
-         senderSignature: senderSignature.data,
-     });
+  // Sign the transaction with orchestrator signer that was specified when we created the safeSDK
+  const senderSignature = await safeSDK.signHash(safeTxHash);
+
+  // Propose the transaction which can be signed by other owners via the Safe UI
+  await safeService.proposeTransaction({
+    safeAddress: safeAddress,
+    safeTransactionData: safeTransaction.data,
+    safeTxHash: safeTxHash,
+    senderAddress: signerAddress,
+    senderSignature: senderSignature.data,
+  });
 }
 
 /**
  * Function to create the transaction data for disabling permissioned prover mode
  * @returns {string} - Encoded transaction data
  */
-function createDisablePermissionedProverTxData(): string{
-    // Define the ABI of the function to be called
-    const abi = [
-        "function disablePermissionedProverMode()"
-    ];
+function createDisablePermissionedProverTxData(): string {
+  // Define the ABI of the function to be called
+  const abi = ["function disablePermissionedProverMode()"];
 
-    // Encode the function call with the provided prover address
-    const data = new ethers.Interface(abi).encodeFunctionData("disablePermissionedProverMode", []);
-    return data; // Return the encoded transaction data
+  // Encode the function call with the provided prover address
+  const data = new ethers.Interface(abi).encodeFunctionData("disablePermissionedProverMode", []);
+  return data; // Return the encoded transaction data
 }
 
 /**
@@ -98,15 +97,19 @@ function createDisablePermissionedProverTxData(): string{
  * @param {string} value - The value to be sent with the transaction
  * @returns {Promise<any>} - A promise that resolves to the Safe transaction object
  */
-async function createSafeTransaction(safeSDK: Safe, contractAddress: string, data: string, value: string): Promise<any> {
-    // Prepare the safe transaction data with the contract address, data, and value
-    let safeTransactionData = createSafeTransactionData(contractAddress, data, value);
+async function createSafeTransaction(
+  safeSDK: Safe,
+  contractAddress: string,
+  data: string,
+  value: string,
+): Promise<any> {
+  // Prepare the safe transaction data with the contract address, data, and value
+  let safeTransactionData = createSafeTransactionData(contractAddress, data, value);
 
-    // Create the safe transaction using the Safe SDK
-    const safeTransaction = await safeSDK.createTransaction({ transactions: [safeTransactionData] });
+  // Create the safe transaction using the Safe SDK
+  const safeTransaction = await safeSDK.createTransaction({ transactions: [safeTransactionData] });
 
-    return safeTransaction;
+  return safeTransaction;
 }
-
 
 main();
--- contracts/script/multisigTransactionProposals/safeSDK/utils.ts
+++ contracts/script/multisigTransactionProposals/safeSDK/utils.ts
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers'; // Import ethers from the ethers library
+import { ethers } from "ethers"; // Import ethers from the ethers library
 
 /**
  * Function to check if a given string is a valid Ethereum address
@@ -6,10 +6,10 @@ import { ethers } from 'ethers'; // Import ethers from the ethers library
  * @returns {boolean} - Returns true if the address is valid, throws an error otherwise
  */
 export function isValidEthereumAddress(address: string) {
-    if (!ethers.isAddress(address)) {
-        throw new Error("Invalid Ethereum address format"); // Throw an error if the address is invalid
-    }
-    return true; // Return true if the address is valid
+  if (!ethers.isAddress(address)) {
+    throw new Error("Invalid Ethereum address format"); // Throw an error if the address is invalid
+  }
+  return true; // Return true if the address is valid
 }
 
 /**
@@ -18,12 +18,12 @@ export function isValidEthereumAddress(address: string) {
  * @returns {string} - Returns the value of the environment variable
  */
 export function getEnvVar(name: string): string {
-    const value = process.env[name]; // Retrieve the environment variable value
-    // Check if the environment variable is undefined or empty
-    if (value === undefined || value === '') {
-        throw new Error(`Environment variable ${name} is not set`); // Throw an error if the environment variable is not set
-    }
-    return value; // Return the value of the environment variable
+  const value = process.env[name]; // Retrieve the environment variable value
+  // Check if the environment variable is undefined or empty
+  if (value === undefined || value === "") {
+    throw new Error(`Environment variable ${name} is not set`); // Throw an error if the environment variable is not set
+  }
+  return value; // Return the value of the environment variable
 }
 
 /**
@@ -34,20 +34,20 @@ export function getEnvVar(name: string): string {
  * @returns {object} - Returns the safe transaction data object
  */
 export function createSafeTransactionData(to: string, data: string, value: string) {
-    // Check if the destination address is specified
-    if (to == '') {
-        throw new Error("must specify destination address"); // Throw an error if the destination address is not specified
-    }
-    // Check if both data and value are empty
-    if (data == '' && value == '') {
-        throw new Error("Either the contract data or value to be sent must be specified"); // Throw an error if both data and value are empty
-    }
-    isValidEthereumAddress(to); // Validate the destination address
-    // Create the safe transaction data object
-    const safeTransactionData = {
-        to: to,
-        data: data,
-        value: value
-    }
-    return safeTransactionData; // Return the safe transaction data object
+  // Check if the destination address is specified
+  if (to == "") {
+    throw new Error("must specify destination address"); // Throw an error if the destination address is not specified
+  }
+  // Check if both data and value are empty
+  if (data == "" && value == "") {
+    throw new Error("Either the contract data or value to be sent must be specified"); // Throw an error if both data and value are empty
+  }
+  isValidEthereumAddress(to); // Validate the destination address
+  // Create the safe transaction data object
+  const safeTransactionData = {
+    to: to,
+    data: data,
+    value: value,
+  };
+  return safeTransactionData; // Return the safe transaction data object
 }
