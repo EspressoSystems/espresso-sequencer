75e6dc0a6f595d8abe897f9b715a6d859946764b
Rob <rob@espressosys.com>
2024-06-12 12:51:07 -0400

    bootstrap from env variable

--- sequencer/src/network/libp2p.rs
+++ sequencer/src/network/libp2p.rs
@@ -1,38 +1,14 @@
 use anyhow::Result;
 use libp2p::{multiaddr::Protocol, Multiaddr, PeerId};
-use serde::{Deserialize, Serialize};
 
-/// A bootstrap node. Contains the multiaddress and peer ID
-#[derive(Clone, Debug, Serialize, Deserialize)]
-#[serde(try_from = "Multiaddr", into = "Multiaddr")]
-pub struct BootstrapNode {
-    pub address: Multiaddr,
-    pub peer_id: PeerId,
-}
+/// Split off the peer ID from a multiaddress, returning the shortened address and the peer ID.
+///
+/// # Errors
+/// - If the last protocol in the address is not a peer ID.
+pub fn split_off_peer_id(mut address: Multiaddr) -> Result<(PeerId, Multiaddr)> {
+    let Some(Protocol::P2p(peer_id)) = address.pop() else {
+        return Err(anyhow::anyhow!("Failed to parse peer ID from address"));
+    };
 
-impl From<BootstrapNode> for Multiaddr {
-    fn from(node: BootstrapNode) -> Self {
-        let mut address = node.address;
-
-        // The standard is to have the peer ID as the last part of the address
-        address.push(Protocol::P2p(node.peer_id));
-
-        address
-    }
-}
-
-impl TryFrom<Multiaddr> for BootstrapNode {
-    type Error = anyhow::Error;
-
-    fn try_from(address: Multiaddr) -> Result<Self> {
-        // Clone the address so we can pop the peer ID off the end
-        let mut address = address.clone();
-
-        // The standard is to have the peer ID as the last part of the address
-        let Some(Protocol::P2p(peer_id)) = address.pop() else {
-            return Err(anyhow::anyhow!("Failed to parse peer ID from address"));
-        };
-
-        Ok(BootstrapNode { address, peer_id })
-    }
+    Ok((peer_id, address))
 }
