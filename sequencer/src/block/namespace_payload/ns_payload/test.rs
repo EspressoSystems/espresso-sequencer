dea3447a5c9314bc21ecfa77d317c404d58717a2
Gus Gutoski <ggutoski@users.noreply.github.com>
2024-06-26 16:52:20 -0400

    new test stub short_ns_payload

---
+++ sequencer/src/block/namespace_payload/ns_payload/test.rs
@@ -0,0 +1,51 @@
+use super::NsPayloadOwned;
+use crate::{
+    block::namespace_payload::NsPayloadBuilder, block::uint_bytes::usize_to_bytes, NamespaceId,
+};
+use async_compatibility_layer::logging::{setup_backtrace, setup_logging};
+
+#[test]
+fn short_ns_payload() {
+    // play with this
+    let entries = [10, 20, 30];
+
+    setup_logging();
+    setup_backtrace();
+    let ns_id = NamespaceId::from(69); // dummy
+
+    // ordinary valid ns_payload
+    let ns_payload = tx_table_with_body(&entries, *entries.iter().max().unwrap_or(&0));
+    let mut tx_count = 0;
+    let mut prev_entry = 0;
+    for (i, tx_index) in ns_payload.iter().enumerate() {
+        let tx = ns_payload.export_tx(&ns_id, &tx_index).unwrap();
+        assert_eq!(tx.payload().len(), entries[i] - prev_entry);
+        tx_count += 1;
+        prev_entry = entries[i];
+    }
+    assert_eq!(tx_count, entries.len());
+
+    // TODO:
+    // tx table offsets out of bounds for payload
+    // small payload can't fit the whole tx table
+    // extremely small payload can't even fit the tx table header
+}
+
+fn tx_table(entries: &[usize]) -> NsPayloadOwned {
+    let mut bytes = Vec::new();
+    bytes.extend(usize_to_bytes::<
+        { NsPayloadBuilder::tx_table_header_byte_len() },
+    >(entries.len()));
+    for entry in entries {
+        bytes.extend(usize_to_bytes::<
+            { NsPayloadBuilder::tx_table_entry_byte_len() },
+        >(*entry));
+    }
+    NsPayloadOwned(bytes)
+}
+
+fn tx_table_with_body(entries: &[usize], body_byte_len: usize) -> NsPayloadOwned {
+    let mut bytes = tx_table(entries).0;
+    bytes.extend(vec![42; body_byte_len]);
+    NsPayloadOwned(bytes)
+}
