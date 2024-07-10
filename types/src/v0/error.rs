27e928fe67ab4e5031bd54d45269fd100139cb4d
Abdul Basit <baronbasit@gmail.com>
2024-07-10 18:32:48 +0500

    add comment for global error issue

--- types/src/v0/error.rs
+++ types/src/v0/error.rs
@@ -3,7 +3,8 @@ use core::fmt::Debug;
 use serde::{Deserialize, Serialize};
 use snafu::Snafu;
 
-// TODO: Remove issue #1681 (https://github.com/EspressoSystems/espresso-sequencer/issues/1681)
+// TODO: Remove global error
+// issue #1681 (https://github.com/EspressoSystems/espresso-sequencer/issues/1681)
 #[derive(Clone, Debug, Snafu, Deserialize, Serialize)]
 pub enum Error {
     // TODO: Can we nest these errors in a `ValidationError` to group them?
