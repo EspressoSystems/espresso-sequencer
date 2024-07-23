fc1e00af177172e4bec52ed17cdf701b93d911f1
bfish713 <bfish713@users.noreply.github.com>
2024-07-23 17:42:23 +0000

    deploy: a9db44e23829c6081d94bf721786298eef4403ad

--- trait.impl/hotshot_types/traits/auction_results_provider/trait.HasUrl.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"espresso_types":[["impl HasUrl for <a class=\"struct\" href=\"espresso_types/v0/v0_3/auction/struct.BidTx.html\" title=\"struct espresso_types::v0::v0_3::auction::BidTx\">BidTx</a>"],["impl HasUrl for <a class=\"struct\" href=\"espresso_types/v0/v0_3/auction/struct.BidTxBody.html\" title=\"struct espresso_types::v0::v0_3::auction::BidTxBody\">BidTxBody</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
