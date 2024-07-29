9a2fad34e076ae08de6ef3186ea7b6b088cda176
Abdul Basit <baronbasit@gmail.com>
2024-07-29 20:52:52 +0500

    base version to 0.3

--- types/src/v0/mod.rs
+++ types/src/v0/mod.rs
@@ -132,8 +132,8 @@ impl NodeType for SeqTypes {
     type ValidatedState = ValidatedState;
     type Membership = GeneralStaticCommittee<Self, PubKey>;
     type BuilderSignatureKey = FeeAccount;
-    type Base = StaticVersion<0, 1>;
-    type Upgrade = StaticVersion<0, 2>;
+    type Base = StaticVersion<0, 3>;
+    type Upgrade = StaticVersion<0, 4>;
     const UPGRADE_HASH: [u8; 32] = [
         1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
         0, 0,
