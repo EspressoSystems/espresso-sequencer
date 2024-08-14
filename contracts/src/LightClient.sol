c1f7e61754f37d7ed59468cd1621ac690571167a
Alysia Huggins <hi@alysia.tech>
2024-08-14 10:17:34 -0400

    abi coder v2 is activated by defauult in solidity v0.8.0^

--- contracts/src/LightClient.sol
+++ contracts/src/LightClient.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
-pragma experimental ABIEncoderV2;
 
 import { OwnableUpgradeable } from
     "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
