63f909902d4abe4ab288fee08af4555f9444d9bd
tbro <tbro@users.noreply.github.com>
2024-08-15 19:40:37 +0000

    deploy: 4c577fb88fd6615884dc6fd5a2e69b3914850e58

---
+++ trait.impl/hotshot_types/traits/node_implementation/trait.Versions.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"espresso_types":[["impl Versions for <a class=\"struct\" href=\"espresso_types/v0/struct.SequencerVersions.html\" title=\"struct espresso_types::v0::SequencerVersions\">SequencerVersions</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
