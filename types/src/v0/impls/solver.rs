07d1119a44193f5ca3348964de73e73d6a37e10d
Abdul Basit <baronbasit@gmail.com>
2024-08-22 19:39:19 +0500

    handle optional fields in rollup update body commit

--- types/src/v0/impls/solver.rs
+++ types/src/v0/impls/solver.rs
@@ -38,28 +38,44 @@ impl Committable for RollupUpdatebody {
             .u64_field("namespace_id", u64::from(self.namespace_id));
 
         if let Some(reserve_url) = &self.reserve_url {
-            comm = comm.var_size_field("reserve_url", reserve_url.as_str().as_ref())
+            comm = comm
+                .u64_field("reserve_url", 1)
+                .var_size_bytes(reserve_url.as_str().as_ref());
+        } else {
+            comm = comm.u64_field("reserve_url", 0);
         }
 
         if let Some(rp) = self.reserve_price {
-            comm = comm.fixed_size_field("reserve_price", &rp.to_fixed_bytes())
-        };
+            comm = comm
+                .u64_field("reserve_price", 1)
+                .fixed_size_bytes(&rp.to_fixed_bytes());
+        } else {
+            comm = comm.u64_field("reserve_price", 0);
+        }
 
         if let Some(active) = self.active {
-            comm = comm.fixed_size_field("active", &[u8::from(active)]);
+            comm = comm
+                .u64_field("active", 1)
+                .fixed_size_bytes(&[u8::from(active)]);
+        } else {
+            comm = comm.u64_field("active", 0);
         }
 
         if let Some(keys) = &self.signature_keys {
-            comm = comm.constant_str("signature_keys");
-            for key in keys.iter() {
+            comm = comm.u64_field("signature_keys", 1);
+            for key in keys {
                 comm = comm.var_size_bytes(&key.to_bytes());
             }
+        } else {
+            comm = comm.u64_field("signature_keys", 0);
         }
 
         comm = comm.var_size_field("signature_key", &self.signature_key.to_bytes());
 
         if let Some(text) = &self.text {
-            comm = comm.var_size_field("text", text.as_bytes());
+            comm = comm.u64_field("text", 1).var_size_bytes(text.as_bytes());
+        } else {
+            comm = comm.u64_field("text", 0);
         }
 
         comm.finalize()
