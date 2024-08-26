bb0be24a184409a05404452985dc002b5a2e2f9b
imabdulbasit <imabdulbasit@users.noreply.github.com>
2024-08-26 21:12:25 +0000

    deploy: adaff0394bf355b3900686a90508ea89e9b1b95d

--- marketplace_solver/api/sidebar-items.js
+++ marketplace_solver/api/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["SolverError"],"fn":["define_api","load_api","load_toml","merge_toml","overflow_err","serde_json_err"],"struct":["ApiOptions"]};
+window.SIDEBAR_ITEMS = {"enum":["SolverError"],"fn":["define_api","load_api","load_toml","merge_toml","overflow_err"],"struct":["ApiOptions"]};
