cbf5b046db0622f4555e2e29f77428edac04ebc2
Abdul Basit <45506001+imabdulbasit@users.noreply.github.com>
2024-08-27 02:45:11 +0500

    handle optional fields in rollup update body commitment (#1890)
    handle optional fields in rollup update body commit

--- types/src/v0/impls/solver.rs
+++ types/src/v0/impls/solver.rs
@@ -5,6 +5,7 @@ use super::{
     v0_3::{RollupRegistrationBody, RollupUpdatebody},
     Update,
 };
+use crate::Update::Set;
 
 impl Committable for RollupRegistrationBody {
     fn tag() -> String {
@@ -49,29 +50,47 @@ impl Committable for RollupUpdatebody {
         let mut comm = committable::RawCommitmentBuilder::new(&Self::tag())
             .u64_field("namespace_id", u64::from(self.namespace_id));
 
-        if let Update::Set(Some(reserve_url)) = &self.reserve_url {
-            comm = comm.var_size_field("reserve_url", reserve_url.as_str().as_ref())
+        match &self.reserve_url {
+            Set(Some(url)) => {
+                comm = comm
+                    .u64_field("reserve_url", 2)
+                    .var_size_bytes(url.as_str().as_ref())
+            }
+            Set(None) => comm = comm.u64_field("reserve_url", 1),
+            Update::Skip => comm = comm.u64_field("reserve_url", 0),
         }
 
-        if let Update::Set(rp) = self.reserve_price {
-            comm = comm.fixed_size_field("reserve_price", &rp.to_fixed_bytes())
-        };
-
-        if let Update::Set(active) = self.active {
-            comm = comm.fixed_size_field("active", &[u8::from(active)]);
+        if let Set(rp) = self.reserve_price {
+            comm = comm
+                .u64_field("reserve_price", 1)
+                .fixed_size_bytes(&rp.to_fixed_bytes());
+        } else {
+            comm = comm.u64_field("reserve_price", 0);
         }
 
-        if let Update::Set(keys) = &self.signature_keys {
-            comm = comm.constant_str("signature_keys");
-            for key in keys.iter() {
+        if let Set(active) = self.active {
+            comm = comm
+                .u64_field("active", 1)
+                .fixed_size_bytes(&[u8::from(active)]);
+        } else {
+            comm = comm.u64_field("active", 0);
+        }
+
+        if let Set(keys) = &self.signature_keys {
+            comm = comm.u64_field("signature_keys", 1);
+            for key in keys {
                 comm = comm.var_size_bytes(&key.to_bytes());
             }
+        } else {
+            comm = comm.u64_field("signature_keys", 0);
         }
 
         comm = comm.var_size_field("signature_key", &self.signature_key.to_bytes());
 
-        if let Update::Set(text) = &self.text {
-            comm = comm.var_size_field("text", text.as_bytes());
+        if let Set(text) = &self.text {
+            comm = comm.u64_field("text", 1).var_size_bytes(text.as_bytes());
+        } else {
+            comm = comm.u64_field("text", 0);
         }
 
         comm.finalize()
