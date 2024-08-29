166ab769e44c0aefc8f124fce355f478b2c29728
Sishan Long <dlsubjam@gmail.com>
2024-08-29 16:31:58 -0700

    merge conflict

--- marketplace/src/testing/basic_test.rs
+++ marketplace/src/testing/basic_test.rs
@@ -1,34 +1,15 @@
-use hotshot_types::{
-    data::{Leaf, QuorumProposal, ViewNumber},
-    message::Proposal,
-    signature_key::BLSPubKey,
-    simple_certificate::{QuorumCertificate, SimpleCertificate, SuccessThreshold},
-    traits::{block_contents::BlockPayload, node_implementation::ConsensusTime},
-};
-
-use crate::builder_state::MessageType;
-
-use std::marker::PhantomData;
+use hotshot_types::data::QuorumProposal;
 
 use async_compatibility_layer::art::async_sleep;
-use async_compatibility_layer::channel::unbounded;
 use async_std::prelude::FutureExt;
-use hotshot::types::SignatureKey;
-use hotshot_types::{simple_vote::QuorumData, traits::block_contents::vid_commitment};
 
-use hotshot_example_types::{
-    block_types::{TestBlockHeader, TestBlockPayload, TestMetadata, TestTransaction},
-    state_types::{TestInstanceState, TestValidatedState},
-};
+use hotshot_example_types::block_types::TestTransaction;
 
-use crate::utils::BuilderStateId;
+use crate::{builder_state::TransactionSource, testing::TestTypes};
 use crate::{
-    builder_state::{DaProposalMessage, QuorumProposalMessage, RequestMessage, TransactionSource},
-    testing::TestTypes,
+    service::handle_received_txns,
+    testing::{calc_proposal_msg, get_req_msg, start_builder_state},
 };
-use crate::{service::handle_received_txns, testing::start_builder_state};
-use committable::Committable;
-use std::sync::Arc;
 use std::time::Duration;
 
 /// This test simulates multiple builder states receiving messages from the channels and processing them
@@ -82,89 +63,15 @@ async fn test_builder() {
         // get transactions submitted in previous rounds, [] for genesis
         // and simulate the block built from those
         let transactions = prev_proposed_transactions.take().unwrap_or_default();
-        let txn_commitments = transactions.iter().map(Committable::commit).collect();
-        let encoded_transactions = TestTransaction::encode(&transactions);
-        let block_payload = TestBlockPayload { transactions };
-        let block_vid_commitment = vid_commitment(&encoded_transactions, NUM_STORAGE_NODES);
-        let block_builder_commitment =
-            <TestBlockPayload as BlockPayload<TestTypes>>::builder_commitment(
-                &block_payload,
-                &TestMetadata,
-            );
-
-        // generate key for leader of this round
-        let seed = [round as u8; 32];
-        let (pub_key, private_key) = BLSPubKey::generated_from_seed_indexed(seed, round as u64);
-
-        let da_proposal = Arc::new(DaProposalMessage {
-            view_number: ViewNumber::new(round as u64),
-            txn_commitments,
-            sender: pub_key,
-            builder_commitment: block_builder_commitment.clone(),
-        });
-
-        let block_header = TestBlockHeader {
-            block_number: round as u64,
-            payload_commitment: block_vid_commitment,
-            builder_commitment: block_builder_commitment,
-            timestamp: round as u64,
-        };
-
-        let justify_qc = match prev_quorum_proposal.take() {
-            None => {
-                QuorumCertificate::<TestTypes>::genesis(
-                    &TestValidatedState::default(),
-                    &TestInstanceState::default(),
-                )
-                .await
-            }
-            Some(prev_proposal) => {
-                let prev_justify_qc = &prev_proposal.justify_qc;
-                let quorum_data = QuorumData::<TestTypes> {
-                    leaf_commit: Leaf::from_quorum_proposal(&prev_proposal).commit(),
-                };
-
-                // form a justify qc
-                SimpleCertificate::<TestTypes, QuorumData<TestTypes>, SuccessThreshold>::new(
-                    quorum_data.clone(),
-                    quorum_data.commit(),
-                    ViewNumber::new(round as u64),
-                    prev_justify_qc.signatures.clone(),
-                    PhantomData,
-                )
-            }
-        };
-        tracing::debug!("Iteration: {} justify_qc: {:?}", round, justify_qc);
-
-        let quorum_proposal = QuorumProposal::<TestTypes> {
-            block_header,
-            view_number: ViewNumber::new(round as u64),
-            justify_qc: justify_qc.clone(),
-            upgrade_certificate: None,
-            proposal_certificate: None,
-        };
+        let (quorum_proposal, quorum_proposal_msg, da_proposal_msg, builder_state_id) =
+            calc_proposal_msg(NUM_STORAGE_NODES, round, prev_quorum_proposal, transactions).await;
 
         prev_quorum_proposal = Some(quorum_proposal.clone());
-
-        let qc_signature = <TestTypes as hotshot_types::traits::node_implementation::NodeType>::SignatureKey::sign(
-                        &private_key,
-                        block_vid_commitment.as_ref(),
-                        ).expect("Failed to sign payload commitment while preparing QC proposal");
-
-        let quorum_proposal_msg =
-            MessageType::QuorumProposalMessage(QuorumProposalMessage::<TestTypes> {
-                proposal: Arc::new(Proposal {
-                    data: quorum_proposal.clone(),
-                    signature: qc_signature,
-                    _pd: PhantomData,
-                }),
-                sender: pub_key,
-            });
 
         // send quorum and DA proposals for this round
         senders
             .da_proposal
-            .broadcast(MessageType::DaProposalMessage(Arc::clone(&da_proposal)))
+            .broadcast(da_proposal_msg)
             .await
             .unwrap();
         senders
@@ -173,20 +80,7 @@ async fn test_builder() {
             .await
             .unwrap();
 
-        let (response_sender, response_receiver) = unbounded();
-        let request_message = MessageType::<TestTypes>::RequestMessage(RequestMessage {
-            requested_view_number: ViewNumber::new(round as u64),
-            response_channel: response_sender,
-        });
-
-        let req_msg = (
-            response_receiver,
-            BuilderStateId {
-                parent_commitment: block_vid_commitment,
-                parent_view: ViewNumber::new(round as u64),
-            },
-            request_message,
-        );
+        let req_msg = get_req_msg(round as u64, builder_state_id).await;
 
         // give builder state time to fork
         async_sleep(Duration::from_millis(100)).await;
