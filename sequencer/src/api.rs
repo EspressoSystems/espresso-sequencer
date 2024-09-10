8cdd0bc200d2dc01282297ed678f80512a4c07d0
tbro <tbro@users.noreply.github.com>
2024-09-10 15:39:26 -0600

    re-enable fee time based upgrade test

--- sequencer/src/api.rs
+++ sequencer/src/api.rs
@@ -1490,28 +1490,36 @@ mod test {
         test_upgrade_helper::<MySequencerVersions>(upgrades, MySequencerVersions::new()).await;
     }
 
-    // #[async_std::test]
-    // async fn test_fee_upgrade_time_based() {
-    //     setup_test();
+    #[async_std::test]
+    async fn test_fee_upgrade_time_based() {
+        setup_test();
 
-    //     let now = OffsetDateTime::now_utc().unix_timestamp() as u64;
-    //     test_upgrade_helper(
-    //         UpgradeMode::Time(TimeBasedUpgrade {
-    //             start_proposing_time: Timestamp::from_integer(now).unwrap(),
-    //             stop_proposing_time: Timestamp::from_integer(now + 500).unwrap(),
-    //             start_voting_time: None,
-    //             stop_voting_time: None,
-    //         }),
-    //         UpgradeType::Fee {
-    //             chain_config: ChainConfig {
-    //                 max_block_size: 300.into(),
-    //                 base_fee: 1.into(),
-    //                 ..Default::default()
-    //             },
-    //         },
-    //     )
-    //     .await;
-    // }
+        let now = OffsetDateTime::now_utc().unix_timestamp() as u64;
+
+        let mut upgrades = std::collections::BTreeMap::new();
+        type MySequencerVersions = SequencerVersions<StaticVersion<0, 1>, StaticVersion<0, 2>>;
+
+        let mode = UpgradeMode::Time(TimeBasedUpgrade {
+            start_proposing_time: Timestamp::from_integer(now).unwrap(),
+            stop_proposing_time: Timestamp::from_integer(now + 500).unwrap(),
+            start_voting_time: None,
+            stop_voting_time: None,
+        });
+
+        let upgrade_type = UpgradeType::Fee {
+            chain_config: ChainConfig {
+                max_block_size: 300.into(),
+                base_fee: 1.into(),
+                ..Default::default()
+            },
+        };
+
+        upgrades.insert(
+            <MySequencerVersions as Versions>::Upgrade::VERSION,
+            Upgrade { mode, upgrade_type },
+        );
+        test_upgrade_helper::<MySequencerVersions>(upgrades, MySequencerVersions::new()).await;
+    }
 
     #[async_std::test]
     async fn test_marketplace_upgrade_view_based() {
