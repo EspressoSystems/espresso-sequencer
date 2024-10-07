e56abce5efbc77a7cc73f6a8575583a466074732
tbro <tbro@users.noreply.github.com>
2024-10-07 17:30:44 -0600

    fix versioned builder port

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -91,7 +91,9 @@ impl TestConfig {
         };
 
         let builder_url = if sequencer_version >= 03 {
-            let url = url_from_port(dotenvy::var("ESPRESSO_RESERVE_BUILDER_SERVER_PORT")?)?;
+            let url = url_from_port(dotenvy::var(
+                "ESPRESSO_SUBMIT_TRANSACTIONS_PRIVATE_FALLBACK_PORT",
+            )?)?;
 
             Url::from_str(&url)?
                 .join("bundle_info/builderaddress")
