8bab1bd36efe6a79e5995fe049888cee102aa1ae
Rob <rob@espressosys.com>
2024-10-18 10:50:18 -0400

    cdn upgrade

--- sequencer/src/restart_tests.rs
+++ sequencer/src/restart_tests.rs
@@ -4,7 +4,10 @@ use super::*;
 use anyhow::bail;
 use async_compatibility_layer::art::async_timeout;
 use async_std::task::{sleep, spawn, JoinHandle};
-use cdn_broker::{reexports::crypto::signature::KeyPair, Broker, Config as BrokerConfig};
+use cdn_broker::{
+    reexports::{crypto::signature::KeyPair, def::hook::NoMessageHook},
+    Broker, Config as BrokerConfig,
+};
 use cdn_marshal::{Config as MarshalConfig, Marshal};
 use derivative::Derivative;
 use espresso_types::{
@@ -684,6 +687,9 @@ async fn start_broker(ports: &mut PortPicker, dir: &Path) -> JoinHandle<()> {
             public_key: WrappedSignatureKey(public_key),
             private_key,
         },
+
+        user_message_hook: NoMessageHook,
+        broker_message_hook: NoMessageHook,
 
         ca_cert_path: None,
         ca_key_path: None,
