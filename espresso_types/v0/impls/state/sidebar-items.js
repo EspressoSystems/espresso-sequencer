f1eef470495ac7af41d1d51208b0b869d5434656
sveitser <sveitser@users.noreply.github.com>
2024-10-24 08:25:44 +0000

    deploy: 2c1267ccb18e8c7ec8d2e70d45775d29abbbead4

---
+++ espresso_types/v0/impls/state/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["BuilderValidationError","ProposalValidationError","StateValidationError"],"fn":["_apply_full_transactions","apply_proposal","charge_fee","get_l1_deposits","validate_builder_fee","validate_proposal"],"struct":["ValidatedState"]};
