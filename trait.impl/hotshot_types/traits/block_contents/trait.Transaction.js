bebe74b6610fb95e39d6f0a8b4ba80772352cb81
jbearer <jbearer@users.noreply.github.com>
2024-10-28 19:26:04 +0000

    deploy: 5c7841b1bec4861b1d8bf2186c3c2b642a7ccb02

--- trait.impl/hotshot_types/traits/block_contents/trait.Transaction.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl Transaction for <a class=\"struct\" href=\"espresso_types/v0/v0_1/transaction/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::transaction::Transaction\">Transaction</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[217]}
