39db633660339f1a30e67428cf60c6344dc2cea3
sveitser <sveitser@users.noreply.github.com>
2024-10-30 15:05:57 +0000

    deploy: 3ee0a99aa3583fc4c1f1aed56ea5f5df7a41ba7a

---
+++ espresso_dev_node/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["main","run_dev_node_server"],"struct":["AltChainInfo","ApiState","Args","DevInfo","SetHotshotDownReqBody","SetHotshotUpReqBody"]};
---
+++ marketplace_solver/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["SOLVER_API_PATH"],"enum":["SolverError"],"fn":["define_api","handle_events"],"mod":["api","database","events","mock","options","state","testing"],"struct":["ApiOptions","DatabaseOptions","EventsServiceClient","Options"],"type":["SolverResult"]};
