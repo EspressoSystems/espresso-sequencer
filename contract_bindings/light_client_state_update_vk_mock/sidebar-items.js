f95cd6b0ed384f3e26f46829dadd2ed8cb058d60
tbro <tbro@users.noreply.github.com>
2024-10-31 17:33:08 +0000

    deploy: eac777fa7b7c94971cec7332ca07dbf0ec0e37a8

--- contract_bindings/light_client_state_update_vk_mock/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"mod":["light_client_state_update_vk_mock"]};
--- contract_bindings/plonk_verifier/plonk_verifier/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["__BYTECODE","__DEPLOYED_BYTECODE"],"enum":["PlonkVerifierErrors"],"fn":["__abi"],"static":["PLONKVERIFIER_ABI","PLONKVERIFIER_BYTECODE","PLONKVERIFIER_DEPLOYED_BYTECODE"],"struct":["InvalidPlonkArgs","PlonkVerifier","UnsupportedDegree","VerifyCall","VerifyReturn","WrongPlonkVK"]};
