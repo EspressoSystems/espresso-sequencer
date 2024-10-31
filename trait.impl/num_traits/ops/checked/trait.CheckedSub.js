665f25a9727eccbb3cb69a49a686cfb02edacded
alysiahuggins <alysiahuggins@users.noreply.github.com>
2024-10-31 19:46:43 +0000

    deploy: 29730abedf2ebf84f6a6fe52ce8dd116704b5383

--- trait.impl/num_traits/ops/checked/trait.CheckedSub.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"https://docs.rs/num-traits/0.2/num_traits/ops/checked/trait.CheckedSub.html\" title=\"trait num_traits::ops::checked::CheckedSub\">CheckedSub</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeAmount.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeAmount\">FeeAmount</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[365]}
