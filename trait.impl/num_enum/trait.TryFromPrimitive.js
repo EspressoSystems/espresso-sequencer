7eeb21649c7a4b99068aefe5ff2bb1d8d9068eb9
jbearer <jbearer@users.noreply.github.com>
2024-10-31 19:39:00 +0000

    deploy: 9bdafb66e53244848dacb16d704e96cb0956a2a1

---
+++ trait.impl/num_enum/trait.TryFromPrimitive.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sequencer",[["impl TryFromPrimitive for <a class=\"enum\" href=\"sequencer/network/cdn/enum.Topic.html\" title=\"enum sequencer::network::cdn::Topic\">Topic</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[166]}
