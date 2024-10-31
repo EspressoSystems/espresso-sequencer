b8e8d7afda09e8e7c86d7e34d901892fb5060d5a
QuentinI <QuentinI@users.noreply.github.com>
2024-10-31 21:04:20 +0000

    deploy: a982dd74adb51de376da791f700f58d598a5b334

--- espresso_types/v0/utils/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["BACKOFF_FACTOR","BACKOFF_JITTER","MAX_RETRY_DELAY","MIN_RETRY_DELAY"],"enum":["ParseRatioError","Update"],"fn":["parse_duration","parse_size"],"struct":["BackoffParams","GenesisHeader","ParseDurationError","ParseSizeError","Ratio","Timestamp"]};
