213707a8dc3e860ce8859e47cbbe2e46e0120b42
tbro <tbro@users.noreply.github.com>
2024-11-04 13:52:50 +0000

    deploy: ac21942894ee0052f5fecb570b79cce51aced57f

---
+++ sequencer/state_signature/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["SIGNATURE_STORAGE_CAPACITY"],"fn":["form_light_client_state","hash_bytes_to_field","static_stake_table_commitment"],"mod":["relay_server"],"struct":["StateSignatureMemStorage","StateSigner"],"type":["StakeTableCommitmentType"]};
