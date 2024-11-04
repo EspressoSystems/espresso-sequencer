ae6f5951b91eb8930a21bb9abbd6b9e7d724e7a6
imabdulbasit <imabdulbasit@users.noreply.github.com>
2024-11-04 18:55:36 +0000

    deploy: b952b023eab1364c2b1ee4bf716db565ef8270fd

---
+++ trait.impl/hotshot_types/traits/node_implementation/trait.NodeImplementation.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sequencer",[["impl&lt;N: ConnectedNetwork&lt;<a class=\"type\" href=\"espresso_types/v0/type.PubKey.html\" title=\"type espresso_types::v0::PubKey\">PubKey</a>&gt;, P: <a class=\"trait\" href=\"espresso_types/v0/traits/trait.SequencerPersistence.html\" title=\"trait espresso_types::v0::traits::SequencerPersistence\">SequencerPersistence</a>&gt; NodeImplementation&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"sequencer/struct.Node.html\" title=\"struct sequencer::Node\">Node</a>&lt;N, P&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[619]}
---
+++ trait.impl/tide_disco/method/trait.ReadState.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_dev_node",[["impl&lt;S: Signer + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> + 'static&gt; ReadState for <a class=\"struct\" href=\"espresso_dev_node/struct.ApiState.html\" title=\"struct espresso_dev_node::ApiState\">ApiState</a>&lt;S&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[344]}
