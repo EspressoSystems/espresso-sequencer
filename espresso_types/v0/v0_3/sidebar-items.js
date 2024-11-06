3f7625e02ed46bfed380064b10f09fb8a3d16b2f
tbro <tbro@users.noreply.github.com>
2024-11-06 15:11:36 +0000

    deploy: 1a998c2040fe13aea0e87ddb31663f04ed60abb7

---
+++ espresso_types/v0/v0_3/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["VERSION"],"enum":["FullNetworkTx"],"mod":["auction","chain_config","fee_info","header","solver"],"struct":["BidTx","BidTxBody","ChainConfig","Header","ResolvableChainConfig","RollupRegistration","RollupRegistrationBody","RollupUpdate","RollupUpdatebody","SolverAuctionResults"],"trait":["IterableFeeInfo"]};
