97d3fbbaffa97c0a0638925efc5f876ab4e6de55
jbearer <jbearer@users.noreply.github.com>
2024-11-08 20:20:23 +0000

    deploy: 8beed636ff774d7c69eb9c099c4639467511d02c

---
+++ trait.impl/cdn_proto/def/trait.RunDef.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sequencer",[["impl&lt;TYPES: NodeType&gt; RunDef for <a class=\"struct\" href=\"sequencer/network/cdn/struct.ProductionDef.html\" title=\"struct sequencer::network::cdn::ProductionDef\">ProductionDef</a>&lt;TYPES&gt;"],["impl&lt;TYPES: NodeType&gt; RunDef for <a class=\"struct\" href=\"sequencer/network/cdn/struct.TestingDef.html\" title=\"struct sequencer::network::cdn::TestingDef\">TestingDef</a>&lt;TYPES&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[420]}
