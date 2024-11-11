39cb561d996b4bb477819ab89db86e2d08ca8f08
0xkato <0xkato@users.noreply.github.com>
2024-11-11 19:56:22 +0000

    deploy: 9c3a8b9764aef7a46b8708d8b0abb19bf88bc748

--- trait.impl/ethers_middleware/gas_oracle/trait.GasOracle.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["sequencer_utils",[["impl GasOracle for <a class=\"struct\" href=\"sequencer_utils/blocknative/struct.BlockNative.html\" title=\"struct sequencer_utils::blocknative::BlockNative\">BlockNative</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[200]}
--- trait.impl/hotshot_testing/block_builder/trait.BuilderTask.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["sequencer",[["impl BuilderTask&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"sequencer/testing/struct.MarketplaceBuilderImplementation.html\" title=\"struct sequencer::testing::MarketplaceBuilderImplementation\">MarketplaceBuilderImplementation</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[373]}
