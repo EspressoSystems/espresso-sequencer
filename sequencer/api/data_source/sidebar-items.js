f50458d931de36564afa3e9e1b0aa41404c8973c
jbearer <jbearer@users.noreply.github.com>
2024-11-18 18:48:39 +0000

    deploy: 5ad4da1ffb0398e62b541808a6e9c4f09c953a68

---
+++ sequencer/api/data_source/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":["provider"],"struct":["PublicHotShotConfig","PublicNetworkConfig","PublicValidatorConfig"],"trait":["CatchupDataSource","DataSourceOptions","HotShotConfigDataSource","NodeStateDataSource","SequencerDataSource","StateSignatureDataSource","SubmitDataSource"],"type":["Provider"]};
---
+++ trait.impl/hotshot_types/traits/node_implementation/trait.NodeType.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_types",[["impl NodeType for <a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[169]}
