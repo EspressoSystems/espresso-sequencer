c808970ee745b975a7defa2bb581f19ffab2ff98
sveitser <sveitser@users.noreply.github.com>
2024-11-19 16:55:49 +0000

    deploy: 4c8a9359b951b898c873594f9e9506434123820b

--- sequencer/api/data_source/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":["provider"],"struct":["PublicHotShotConfig","PublicNetworkConfig","PublicValidatorConfig"],"trait":["CatchupDataSource","DataSourceOptions","HotShotConfigDataSource","NodeStateDataSource","SequencerDataSource","StateSignatureDataSource","SubmitDataSource"],"type":["Provider"]};
--- trait.impl/hotshot_types/traits/signature_key/trait.PrivateSignatureKey.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl PrivateSignatureKey for <a class=\"struct\" href=\"espresso_types/eth_signature_key/struct.EthKeyPair.html\" title=\"struct espresso_types::eth_signature_key::EthKeyPair\">EthKeyPair</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[216]}
