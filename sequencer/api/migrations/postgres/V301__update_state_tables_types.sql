993aba6380f493b499953d676f2c7a0778a4f648
Abdul Basit <baronbasit@gmail.com>
2024-11-20 04:04:52 +0500

    fix migration

--- sequencer/api/migrations/postgres/V301__update_state_tables_types.sql
+++ sequencer/api/migrations/postgres/V301__update_state_tables_types.sql
@@ -10,4 +10,4 @@ ALTER TABLE block_merkle_tree
   ALTER COLUMN path TYPE JSONB USING array_to_json(path),
   ALTER COLUMN children TYPE JSONB USING array_to_json(children);
 
-ALTER TABLE fee_merkle_tree RENAME COLUMN index TO idx;
+ALTER TABLE block_merkle_tree RENAME COLUMN index TO idx;
