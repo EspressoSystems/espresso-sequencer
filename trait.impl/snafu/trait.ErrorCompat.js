a1746fdac8c5a898155f035f0498c11dd2486e8e
sveitser <sveitser@users.noreply.github.com>
2024-11-19 16:56:04 +0000

    deploy: 4c8a9359b951b898c873594f9e9506434123820b

---
+++ trait.impl/snafu/trait.ErrorCompat.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["orchestrator",[["impl ErrorCompat for <a class=\"enum\" href=\"orchestrator/enum.ParseSeedError.html\" title=\"enum orchestrator::ParseSeedError\">ParseSeedError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[171]}
