0441757a739319c9cd28db1dde048bf2126ffcfd
jbearer <jbearer@users.noreply.github.com>
2024-11-19 20:20:29 +0000

    deploy: 239e3e27a91fe961f94eea0300f30291db7019ad

--- trait.impl/hotshot_types/traits/node_implementation/trait.HasUrls.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl HasUrls for <a class=\"struct\" href=\"espresso_types/v0/v0_3/auction/struct.SolverAuctionResults.html\" title=\"struct espresso_types::v0::v0_3::auction::SolverAuctionResults\">SolverAuctionResults</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[232]}
--- trait.impl/hotshot_types/traits/states/trait.InstanceState.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl InstanceState for <a class=\"struct\" href=\"espresso_types/v0/impls/instance_state/struct.NodeState.html\" title=\"struct espresso_types::v0::impls::instance_state::NodeState\">NodeState</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[221]}
