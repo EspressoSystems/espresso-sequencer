dbc272c01dc4d57b9c1985a60932171a7a6bb192
0xkato <0xkato@users.noreply.github.com>
2024-11-24 21:48:17 +0000

    deploy: e7e7d55aea2ef85f4c4ed6b426c224764563aa80

---
+++ trait.impl/hotshot_types/traits/states/trait.ValidatedState.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_types",[["impl ValidatedState&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"espresso_types/v0/impls/state/struct.ValidatedState.html\" title=\"struct espresso_types::v0::impls::state::ValidatedState\">ValidatedState</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[353]}
