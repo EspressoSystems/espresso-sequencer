fc90c2d6a393386364ad5f8451832afea66bc788
sveitser <sveitser@users.noreply.github.com>
2024-11-26 08:53:13 +0000

    deploy: 14ae7719c4dc5319586c991c8c3100f0fbe5b1f6

--- trait.impl/sequencer_utils/ser/trait.FromStringOrInteger.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"sequencer_utils/ser/trait.FromStringOrInteger.html\" title=\"trait sequencer_utils::ser::FromStringOrInteger\">FromStringOrInteger</a> for <a class=\"struct\" href=\"espresso_types/v0/utils/struct.Timestamp.html\" title=\"struct espresso_types::v0::utils::Timestamp\">Timestamp</a>"],["impl <a class=\"trait\" href=\"sequencer_utils/ser/trait.FromStringOrInteger.html\" title=\"trait sequencer_utils::ser::FromStringOrInteger\">FromStringOrInteger</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/chain_config/struct.BlockSize.html\" title=\"struct espresso_types::v0::v0_1::chain_config::BlockSize\">BlockSize</a>"],["impl <a class=\"trait\" href=\"sequencer_utils/ser/trait.FromStringOrInteger.html\" title=\"trait sequencer_utils::ser::FromStringOrInteger\">FromStringOrInteger</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/chain_config/struct.ChainId.html\" title=\"struct espresso_types::v0::v0_1::chain_config::ChainId\">ChainId</a>"],["impl <a class=\"trait\" href=\"sequencer_utils/ser/trait.FromStringOrInteger.html\" title=\"trait sequencer_utils::ser::FromStringOrInteger\">FromStringOrInteger</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeAmount.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeAmount\">FeeAmount</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[1349]}
