49e6c0470110489f8e6470bd1b493dcb9e897947
tbro <tbro@users.noreply.github.com>
2024-11-27 16:30:53 +0000

    deploy: e03304d9e70a42ae31dc9f3224941717d8a6f50e

--- hotshot_state_prover/mock_ledger/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"constant":["STAKE_TABLE_CAPACITY"],"fn":["gen_circuit_for_test","gen_plonk_proof_for_test","key_pairs_for_testing","stake_table_for_testing"],"struct":["MockLedger","MockSystemParam"],"type":["F","SchnorrSignKey","SchnorrVerKey"]};
--- trait.impl/ethers_providers/rpc/pubsub/trait.PubsubClient.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl PubsubClient for <a class=\"enum\" href=\"espresso_types/v0/v0_1/l1/enum.RpcClient.html\" title=\"enum espresso_types::v0::v0_1::l1::RpcClient\">RpcClient</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[188]}
