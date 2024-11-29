1d0de398222de38152d7db20835574cc2e70e0f7
rob-maron <rob-maron@users.noreply.github.com>
2024-11-29 19:05:14 +0000

    deploy: f999be4264920823994ccce13f1667ff1c800ef0

---
+++ trait.impl/ethers_core/abi/tokens/trait.Tokenize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hotshot_contract_adapter",[["impl Tokenize for <a class=\"struct\" href=\"hotshot_contract_adapter/light_client/struct.LightClientConstructorArgs.html\" title=\"struct hotshot_contract_adapter::light_client::LightClientConstructorArgs\">LightClientConstructorArgs</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[273]}
