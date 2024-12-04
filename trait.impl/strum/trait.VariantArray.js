7fcf3b216d8f1818db97486803b026a9a735adfc
imabdulbasit <imabdulbasit@users.noreply.github.com>
2024-12-04 21:31:56 +0000

    deploy: 306f2ee77799f9801d52f552d7eafc47368f7a6a

---
+++ trait.impl/strum/trait.VariantArray.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nasty_client",[["impl VariantArray for <a class=\"enum\" href=\"nasty_client/enum.ActionDiscriminants.html\" title=\"enum nasty_client::ActionDiscriminants\">ActionDiscriminants</a>"],["impl VariantArray for <a class=\"enum\" href=\"nasty_client/enum.Resource.html\" title=\"enum nasty_client::Resource\">Resource</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[323]}
