295612c9c3c59fe2470ed5d5d25e7f34ee38990e
jbearer <jbearer@users.noreply.github.com>
2024-12-04 16:02:32 +0000

    deploy: abf9a9b7122a6a178b6c5d484854723102ac9912

---
+++ espresso_types/v0/traits/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["NullEventConsumer"],"trait":["EventConsumer","FromNsPayloadBytes","FromStringOrInteger","NsPayloadBytesRange","PersistenceOptions","SequencerPersistence","StateCatchup"]};
