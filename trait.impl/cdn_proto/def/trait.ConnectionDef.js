ca178bd8b922cd29c6ae2879031b3ea1f99d3ad5
QuentinI <QuentinI@users.noreply.github.com>
2024-12-06 17:12:28 +0000

    deploy: 4a09be2feff17e2cf311d49f3190e5b1fe066f35

--- trait.impl/cdn_proto/def/trait.ConnectionDef.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["sequencer",[["impl&lt;TYPES: NodeType&gt; ConnectionDef for <a class=\"struct\" href=\"sequencer/network/cdn/struct.BrokerDef.html\" title=\"struct sequencer::network::cdn::BrokerDef\">BrokerDef</a>&lt;TYPES&gt;"],["impl&lt;TYPES: NodeType&gt; ConnectionDef for <a class=\"struct\" href=\"sequencer/network/cdn/struct.ClientDef.html\" title=\"struct sequencer::network::cdn::ClientDef\">ClientDef</a>&lt;TYPES&gt;"],["impl&lt;TYPES: NodeType&gt; ConnectionDef for <a class=\"struct\" href=\"sequencer/network/cdn/struct.UserDefQuic.html\" title=\"struct sequencer::network::cdn::UserDefQuic\">UserDefQuic</a>&lt;TYPES&gt;"],["impl&lt;TYPES: NodeType&gt; ConnectionDef for <a class=\"struct\" href=\"sequencer/network/cdn/struct.UserDefTcp.html\" title=\"struct sequencer::network::cdn::UserDefTcp\">UserDefTcp</a>&lt;TYPES&gt;"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[832]}
--- trait.impl/hotshot_types/traits/auction_results_provider/trait.AuctionResultsProvider.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl&lt;TYPES: NodeType&gt; AuctionResultsProvider&lt;TYPES&gt; for <a class=\"struct\" href=\"espresso_types/v0/impls/auction/struct.SolverAuctionResultsProvider.html\" title=\"struct espresso_types::v0::impls::auction::SolverAuctionResultsProvider\">SolverAuctionResultsProvider</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[309]}
