170b12f5fd1da57b3455242097b7d8a0b1fb62d5
pls148 <pls148@users.noreply.github.com>
2024-12-06 20:30:40 +0000

    deploy: 254d060a818b280ed2a3e4b6539614bd3ffdaa9f

---
+++ trait.impl/ark_serialize/trait.CanonicalSerialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_types",[["impl CanonicalSerialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeAccount.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeAccount\">FeeAccount</a>"],["impl CanonicalSerialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeAmount.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeAmount\">FeeAmount</a>"],["impl CanonicalSerialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeInfo.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeInfo\">FeeInfo</a>"],["impl CanonicalSerialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/transaction/struct.NamespaceId.html\" title=\"struct espresso_types::v0::v0_1::transaction::NamespaceId\">NamespaceId</a>"],["impl CanonicalSerialize for <a class=\"struct\" href=\"espresso_types/v0/v0_1/transaction/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::transaction::Transaction\">Transaction</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1001]}
---
+++ trait.impl/hotshot_query_service/merklized_state/data_source/trait.MerklizedState.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_types",[["impl MerklizedState&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>, { Self::ARITY }&gt; for <a class=\"type\" href=\"espresso_types/v0/v0_1/state/type.BlockMerkleTree.html\" title=\"type espresso_types::v0::v0_1::state::BlockMerkleTree\">BlockMerkleTree</a>"],["impl MerklizedState&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>, { Self::ARITY }&gt; for <a class=\"type\" href=\"espresso_types/v0/v0_1/state/type.FeeMerkleTree.html\" title=\"type espresso_types::v0::v0_1::state::FeeMerkleTree\">FeeMerkleTree</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[704]}
