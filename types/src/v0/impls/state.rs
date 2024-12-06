dbaca7c08651573a623b740e1805c15c35f54b49
tbro <tbro@users.noreply.github.com>
2024-12-06 14:55:28 -0300

    export local `StaticCommittee`
    and move versions back

--- types/src/v0/impls/state.rs
+++ types/src/v0/impls/state.rs
@@ -31,8 +31,7 @@ use super::{
 };
 use crate::{
     traits::StateCatchup,
-    v0_3::{ChainConfig, ResolvableChainConfig},
-    v0_99::{FullNetworkTx, IterableFeeInfo},
+    v0_99::{ChainConfig, FullNetworkTx, IterableFeeInfo, ResolvableChainConfig},
     BlockMerkleTree, Delta, FeeAccount, FeeAmount, FeeInfo, FeeMerkleTree, Header, Leaf2,
     NsTableValidationError, PayloadByteLen, SeqTypes, UpgradeType, BLOCK_MERKLE_TREE_HEIGHT,
     FEE_MERKLE_TREE_HEIGHT,
