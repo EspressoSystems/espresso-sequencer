0650629e1884043b58e84c3485348068ca44b926
pls148 <pls148@users.noreply.github.com>
2024-12-06 20:30:33 +0000

    deploy: 254d060a818b280ed2a3e4b6539614bd3ffdaa9f

--- trait.impl/marketplace_builder_core/hooks/trait.BuilderHooks.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["marketplace_builder",[["impl BuilderHooks&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"marketplace_builder/hooks/struct.EspressoFallbackHooks.html\" title=\"struct marketplace_builder::hooks::EspressoFallbackHooks\">EspressoFallbackHooks</a>"],["impl BuilderHooks&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"marketplace_builder/hooks/struct.EspressoReserveHooks.html\" title=\"struct marketplace_builder::hooks::EspressoReserveHooks\">EspressoReserveHooks</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[706]}
