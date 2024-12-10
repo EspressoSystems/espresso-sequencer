5bd2a59e341950a8d7798e4627da9872583f5fa3
pls148 <pls148@users.noreply.github.com>
2024-12-10 16:53:38 +0000

    deploy: bc5b7f15c0f9567cca4ed76f05f23fec246cd222

---
+++ contract_bindings/plonk_verifier/plonk_verifier/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":["__BYTECODE","__DEPLOYED_BYTECODE"],"enum":["PlonkVerifierErrors"],"fn":["__abi"],"static":["PLONKVERIFIER_ABI","PLONKVERIFIER_BYTECODE","PLONKVERIFIER_DEPLOYED_BYTECODE"],"struct":["InvalidPlonkArgs","PlonkVerifier","UnsupportedDegree","VerifyCall","VerifyReturn","WrongPlonkVK"]};
