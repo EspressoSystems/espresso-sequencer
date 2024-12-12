02aa75b4cef75cad7ccae0efe9a6f860903d2c8a
sveitser <sveitser@users.noreply.github.com>
2024-12-12 09:10:04 +0000

    deploy: 0fed49e3a444033e965392b7beed5ebb9a9a5e0c

---
+++ trait.impl/ethers_contract/error/trait.ContractRevert.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["contract_bindings",[["impl ContractRevert for <a class=\"enum\" href=\"contract_bindings/erc1967_proxy/erc1967_proxy/enum.ERC1967ProxyErrors.html\" title=\"enum contract_bindings::erc1967_proxy::erc1967_proxy::ERC1967ProxyErrors\">ERC1967ProxyErrors</a>"],["impl ContractRevert for <a class=\"enum\" href=\"contract_bindings/fee_contract/fee_contract/enum.FeeContractErrors.html\" title=\"enum contract_bindings::fee_contract::fee_contract::FeeContractErrors\">FeeContractErrors</a>"],["impl ContractRevert for <a class=\"enum\" href=\"contract_bindings/light_client/light_client/enum.LightClientErrors.html\" title=\"enum contract_bindings::light_client::light_client::LightClientErrors\">LightClientErrors</a>"],["impl ContractRevert for <a class=\"enum\" href=\"contract_bindings/light_client_mock/light_client_mock/enum.LightClientMockErrors.html\" title=\"enum contract_bindings::light_client_mock::light_client_mock::LightClientMockErrors\">LightClientMockErrors</a>"],["impl ContractRevert for <a class=\"enum\" href=\"contract_bindings/permissioned_stake_table/permissioned_stake_table/enum.PermissionedStakeTableErrors.html\" title=\"enum contract_bindings::permissioned_stake_table::permissioned_stake_table::PermissionedStakeTableErrors\">PermissionedStakeTableErrors</a>"],["impl ContractRevert for <a class=\"enum\" href=\"contract_bindings/plonk_verifier/plonk_verifier/enum.PlonkVerifierErrors.html\" title=\"enum contract_bindings::plonk_verifier::plonk_verifier::PlonkVerifierErrors\">PlonkVerifierErrors</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1531]}
---
+++ trait.impl/ethers_contract/log/trait.EthLogDecode.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["contract_bindings",[["impl EthLogDecode for <a class=\"enum\" href=\"contract_bindings/fee_contract/fee_contract/enum.FeeContractEvents.html\" title=\"enum contract_bindings::fee_contract::fee_contract::FeeContractEvents\">FeeContractEvents</a>"],["impl EthLogDecode for <a class=\"enum\" href=\"contract_bindings/light_client/light_client/enum.LightClientEvents.html\" title=\"enum contract_bindings::light_client::light_client::LightClientEvents\">LightClientEvents</a>"],["impl EthLogDecode for <a class=\"enum\" href=\"contract_bindings/light_client_mock/light_client_mock/enum.LightClientMockEvents.html\" title=\"enum contract_bindings::light_client_mock::light_client_mock::LightClientMockEvents\">LightClientMockEvents</a>"],["impl EthLogDecode for <a class=\"enum\" href=\"contract_bindings/permissioned_stake_table/permissioned_stake_table/enum.PermissionedStakeTableEvents.html\" title=\"enum contract_bindings::permissioned_stake_table::permissioned_stake_table::PermissionedStakeTableEvents\">PermissionedStakeTableEvents</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1044]}
