5ceefc7b43bae5aa2f14c190b0bb289bcf308d0f
sveitser <sveitser@users.noreply.github.com>
2024-12-12 09:18:34 +0000

    deploy: 214e46c622aaa83a7e849ee4b805570ff5732065

---
+++ espresso_types/v0/v0_1/l1/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["L1Event","L1ReconnectTask","RpcClient"],"struct":["L1BlockInfo","L1Client","L1ClientMetrics","L1ClientOptions","L1Snapshot","L1State","L1UpdateTask"]};
