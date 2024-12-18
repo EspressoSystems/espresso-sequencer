fd17810801442eb5fcde176a09c97ee296d25259
sveitser <sveitser@gmail.com>
2024-12-18 13:18:53 +0100

    Add script to benchmark compilation times

--- flake.nix
+++ flake.nix
@@ -245,7 +245,7 @@
             solc
             nodePackages.prettier
             solhint
-            (python3.withPackages (ps: with ps; [ black ]))
+            (python3.withPackages (ps: with ps; [ black tomlkit ]))
             yarn
           ] ++ lib.optionals stdenv.isDarwin
             [ darwin.apple_sdk.frameworks.SystemConfiguration ]
