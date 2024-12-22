9c97b7e0d53527112ecbaf727439c018b0628aef
Sneh Koul <snehkoul1999@gmail.com>
2024-12-22 10:32:08 -0500

    contract-bindings

--- contracts/src/LightClientArbitrum.sol
+++ contracts/src/LightClientArbitrum.sol
@@ -5,11 +5,11 @@ pragma solidity ^0.8.0;
 import { LightClient } from "./LightClient.sol";
 
 interface ArbSys {
-  function arbBlockNumber() external view returns (uint256);
+    function arbBlockNumber() external view returns (uint256);
 }
 
 contract LightClientArbitrum is LightClient {
-  function currentBlockNumber() public view virtual override returns (uint256) {
-    return ArbSys(address(uint160(100))).arbBlockNumber();
-  }
+    function currentBlockNumber() public view virtual override returns (uint256) {
+        return ArbSys(address(uint160(100))).arbBlockNumber();
+    }
 }
