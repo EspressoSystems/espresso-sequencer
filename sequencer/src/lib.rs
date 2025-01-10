1695531d9feaea4b8392dfd68572c5f03c57152f
tbro <tbro@users.noreply.github.com>
2025-01-10 12:08:21 -0300

    pass `DhtNoPerisistane` to `Libp2pNetwork::from_config`

--- sequencer/src/lib.rs
+++ sequencer/src/lib.rs
@@ -26,6 +26,7 @@ use std::sync::Arc;
 use tokio::select;
 // Should move `STAKE_TABLE_CAPACITY` in the sequencer repo when we have variate stake table support
 use libp2p::Multiaddr;
+use libp2p_networking::network::behaviours::dht::store::persistent::DhtNoPersistence;
 use network::libp2p::split_off_peer_id;
 use options::Identity;
 use state_signature::static_stake_table_commitment;
@@ -489,6 +490,7 @@ pub async fn init_node<P: SequencerPersistence, V: Versions>(
     let network = {
         let p2p_network = Libp2pNetwork::from_config(
             network_config.clone(),
+            DhtNoPersistence,
             Arc::new(async_lock::RwLock::new(membership.clone())),
             gossip_config,
             request_response_config,
