c163ffcb1744de1f5d1ae1cc3e95ba4ab7685e74
pls148 <pls148@users.noreply.github.com>
2025-01-10 21:43:58 +0000

    deploy: 561dd5aa3c1aaa70e6faa268b0bedf4c2fbed9f9

--- trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/block/struct.Iter.html\" title=\"struct espresso_types::v0::v0_1::block::Iter\">Iter</a>&lt;'_&gt;"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/block/struct.NsIter.html\" title=\"struct espresso_types::v0::v0_1::block::NsIter\">NsIter</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/block/struct.TxIter.html\" title=\"struct espresso_types::v0::v0_1::block::TxIter\">TxIter</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[1023]}
--- trait.impl/core/ops/arith/trait.Add.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeAmount.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeAmount\">FeeAmount</a>"]]],["node_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.0/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"node_metrics/service/client_id/struct.ClientId.html\" title=\"struct node_metrics::service::client_id::ClientId\">ClientId</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[330,426]}
