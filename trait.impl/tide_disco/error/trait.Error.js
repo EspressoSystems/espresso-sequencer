d952172504140e9275ec85ff5a6863f2c2e68491
pls148 <pls148@users.noreply.github.com>
2025-01-10 21:15:07 +0000

    deploy: cafd98e78a5b1dd1fa56289a5c4890b5bf63d192

--- trait.impl/tide_disco/error/trait.Error.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["marketplace_solver",[["impl Error for <a class=\"enum\" href=\"marketplace_solver/api/enum.SolverError.html\" title=\"enum marketplace_solver::api::SolverError\">SolverError</a>"]]],["node_metrics",[["impl Error for <a class=\"enum\" href=\"node_metrics/api/node_validator/v0/enum.Error.html\" title=\"enum node_metrics::api::node_validator::v0::Error\">Error</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[183,186]}
