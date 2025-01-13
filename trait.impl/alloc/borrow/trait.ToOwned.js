e3f37a1e69373bd16e4833c735f7b2407bff8cdc
rob-maron <rob-maron@users.noreply.github.com>
2025-01-13 22:23:09 +0000

    deploy: 4b6594908322db52d618dd6bc1a85409a8f2f525

--- trait.impl/alloc/borrow/trait.ToOwned.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.0/alloc/borrow/trait.ToOwned.html\" title=\"trait alloc::borrow::ToOwned\">ToOwned</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/block/struct.NsPayload.html\" title=\"struct espresso_types::v0::v0_1::block::NsPayload\">NsPayload</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[331]}
