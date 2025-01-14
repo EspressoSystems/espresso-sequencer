ea5d100035cef62b61a85238840d41d86d633374
Jeb Bearer <jeb.bearer@gmail.com>
2025-01-14 12:14:59 -0800

    L1 client fixup (#2442)
    * Add new L1 env vars to public endpoint
    
    * Don't expose L1 provider URL in metrics

--- types/src/v0/impls/l1.rs
+++ types/src/v0/impls/l1.rs
@@ -131,9 +131,9 @@ impl L1ClientMetrics {
 }
 
 impl L1Provider {
-    fn new(url: Url, failures: &dyn CounterFamily) -> Self {
+    fn new(index: usize, url: Url, failures: &dyn CounterFamily) -> Self {
         Self {
-            failures: failures.create(vec![url.to_string()]),
+            failures: failures.create(vec![index.to_string()]),
             inner: Http::new(url),
         }
     }
@@ -157,7 +157,8 @@ impl MultiRpcClient {
             clients: Arc::new(
                 clients
                     .into_iter()
-                    .map(|url| L1Provider::new(url, &*failures))
+                    .enumerate()
+                    .map(|(i, url)| L1Provider::new(i, url, &*failures))
                     .collect(),
             ),
             status: Default::default(),
@@ -226,12 +227,13 @@ impl JsonRpcClient for MultiRpcClient {
 
             status.client
         };
-        let client = &self.clients[current % self.clients.len()];
+        let provider = current % self.clients.len();
+        let client = &self.clients[provider];
         match client.request(method, &params).await {
             Ok(res) => Ok(res),
             Err(err) => {
                 let t = Instant::now();
-                tracing::warn!(?t, method, ?params, "L1 client error: {err:#}");
+                tracing::warn!(?t, method, ?params, provider, "L1 client error: {err:#}");
                 client.failures.add(1);
 
                 // Keep track of failures, failing over to the next client if necessary.
@@ -344,11 +346,12 @@ impl L1Client {
                         Some(urls) => {
                             // Use a new WebSockets host each time we retry in case there is a
                             // problem with one of the hosts specifically.
-                            let url = &urls[i % urls.len()];
+                            let provider = i % urls.len();
+                            let url = &urls[provider];
                             ws = match Provider::<Ws>::connect(url.clone()).await {
                                 Ok(ws) => ws,
                                 Err(err) => {
-                                    tracing::warn!(%url, "failed to connect WebSockets provider: {err:#}");
+                                    tracing::warn!(provider, "failed to connect WebSockets provider: {err:#}");
                                     sleep(retry_delay).await;
                                     continue;
                                 }
