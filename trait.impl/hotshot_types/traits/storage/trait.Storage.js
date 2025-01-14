5648ebbcdbf01fa93ca370e266a46b31e62e0f93
tbro <tbro@users.noreply.github.com>
2025-01-14 04:35:23 +0000

    deploy: 9d1467df416f99e42c84c52977330e24013c3030

--- trait.impl/hotshot_types/traits/storage/trait.Storage.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl&lt;P: <a class=\"trait\" href=\"espresso_types/v0/traits/trait.SequencerPersistence.html\" title=\"trait espresso_types::v0::traits::SequencerPersistence\">SequencerPersistence</a>&gt; Storage&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"https://doc.rust-lang.org/1.84.0/alloc/sync/struct.Arc.html\" title=\"struct alloc::sync::Arc\">Arc</a>&lt;P&gt;"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[500]}
