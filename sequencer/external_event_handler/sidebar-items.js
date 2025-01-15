d2d19371d1909130db32153801f120fe2da9c6c4
pls148 <pls148@users.noreply.github.com>
2025-01-15 17:01:54 +0000

    deploy: 2bb102e1959f6e62e2dc79d885a0b5d7e571f823

---
+++ sequencer/external_event_handler/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["ExternalMessage","OutboundMessage"],"struct":["ExternalEventHandler","RollCallInfo"]};
---
+++ trait.impl/hotshot_types/traits/node_implementation/trait.Versions.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_types",[["impl&lt;Base: StaticVersionType + 'static, Upgrade: StaticVersionType + 'static&gt; Versions for <a class=\"struct\" href=\"espresso_types/v0/struct.SequencerVersions.html\" title=\"struct espresso_types::v0::SequencerVersions\">SequencerVersions</a>&lt;Base, Upgrade&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[296]}
