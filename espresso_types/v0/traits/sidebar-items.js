363b76886d8af1586439d89fbdbab12f3793d08c
alysiahuggins <alysiahuggins@users.noreply.github.com>
2025-01-15 16:17:02 +0000

    deploy: 5eaddb040e06570f56b972a777c27585f925ef63

--- espresso_types/v0/traits/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"struct":["NullEventConsumer"],"trait":["EventConsumer","FromNsPayloadBytes","FromStringOrInteger","NsPayloadBytesRange","PersistenceOptions","SequencerPersistence","StateCatchup"]};
--- trait.impl/hotshot_types/traits/election/trait.Membership.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl Membership&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"espresso_types/v0/impls/stake_table/struct.EpochCommittees.html\" title=\"struct espresso_types::v0::impls::stake_table::EpochCommittees\">EpochCommittees</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[364]}
