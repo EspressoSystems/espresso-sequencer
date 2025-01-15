7c70fbf1d8ed46554b89d905825f7480f79d46f1
alysiahuggins <alysiahuggins@users.noreply.github.com>
2025-01-15 16:17:39 +0000

    deploy: 5eaddb040e06570f56b972a777c27585f925ef63

---
+++ espresso_types/v0/v0_1/instance_state/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["UpgradeMode","UpgradeType"],"struct":["TimeBasedUpgrade","Upgrade","ViewBasedUpgrade"]};
---
+++ search.desc/state_relay_server/state_relay_server-desc-0-.js
@@ -0,0 +1,1 @@
+searchState.loadedDescShard("state_relay_server", 0, "Returns the argument unchanged.\nCalls <code>U::from(self)</code>.\nPort to run the server on.\nTotal amount of stake. WARNING: this is a temporary flag, …")
