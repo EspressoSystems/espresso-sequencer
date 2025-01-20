ba2c6e51ee61aed261c750a0794b96cfd433a5c3
sveitser <sveitser@users.noreply.github.com>
2025-01-20 11:18:42 +0000

    deploy: 018fbc50b268e87828e1f9da50dbd4c242c66672

--- trait.impl/hotshot_types/traits/node_implementation/trait.NodeType.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl NodeType for <a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[169]}
