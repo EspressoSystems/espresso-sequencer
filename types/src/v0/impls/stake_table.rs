3a807d73edb3dcaf17e95552e27053edabe47673
ss-es <155648797+ss-es@users.noreply.github.com>
2025-01-29 17:19:00 -0500

    fix da membership

--- types/src/v0/impls/stake_table.rs
+++ types/src/v0/impls/stake_table.rs
@@ -201,7 +201,7 @@ impl EpochCommittees {
         let da_members: Vec<_> = da_members
             .iter()
             .map(|member| member.stake_table_entry.clone())
-            .filter(|entry| entry.stake() > U256::zero())
+            .filter(|entry| (entry.stake() > U256::zero()) && entry.da)
             .collect();
 
         // Index the stake table by public key
