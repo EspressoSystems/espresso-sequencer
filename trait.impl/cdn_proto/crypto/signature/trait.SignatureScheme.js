849d7f0e5f879818f247918af233ed632bb8677b
jparr721 <jparr721@users.noreply.github.com>
2025-01-31 16:03:25 +0000

    deploy: 45ba9300d64026460fa0bef3d069d6ffce677ca4

---
+++ trait.impl/cdn_proto/crypto/signature/trait.SignatureScheme.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["sequencer",[["impl&lt;T: SignatureKey&gt; SignatureScheme for <a class=\"struct\" href=\"sequencer/network/cdn/struct.WrappedSignatureKey.html\" title=\"struct sequencer::network::cdn::WrappedSignatureKey\">WrappedSignatureKey</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[245]}
