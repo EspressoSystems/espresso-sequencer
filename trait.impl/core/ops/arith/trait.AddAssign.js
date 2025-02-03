96976b72c90822b229afc05fe1bec99033907fea
sveitser <sveitser@users.noreply.github.com>
2025-02-03 11:31:31 +0000

    deploy: 102ca7eb38dd3293a1265bb8174e247ec84ef896

---
+++ trait.impl/core/ops/arith/trait.AddAssign.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["node_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/arith/trait.AddAssign.html\" title=\"trait core::ops::arith::AddAssign\">AddAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.1/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"node_metrics/service/client_id/struct.ClientId.html\" title=\"struct node_metrics::service::client_id::ClientId\">ClientId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[443]}
