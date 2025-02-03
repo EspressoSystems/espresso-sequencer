3b50e43082ac9315afd3a0eb1330868915090a82
jparr721 <jparr721@users.noreply.github.com>
2025-02-03 22:50:59 +0000

    deploy: b7c10eac3f1f40ced90b7e9aff1231fea95646a4

---
+++ trait.impl/tide_disco/method/trait.ReadState.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hotshot_query_service",[["impl&lt;D: 'static + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/marker/trait.Send.html\" title=\"trait core::marker::Send\">Send</a> + <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a>&gt; ReadState for <a class=\"struct\" href=\"hotshot_query_service/struct.ApiState.html\" title=\"struct hotshot_query_service::ApiState\">ApiState</a>&lt;D&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[481]}
