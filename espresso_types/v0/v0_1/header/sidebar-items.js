c7a893f06f42db6be7ff38dceed84a18c87840ae
sveitser <sveitser@users.noreply.github.com>
2025-02-03 10:59:15 +0000

    deploy: 7d98a218878e37c9c06d38adfd463e37fff33d17

---
+++ espresso_types/v0/v0_1/header/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"macro":["element"],"struct":["Header"]};
---
+++ trait.impl/core/ops/arith/trait.Add.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/fee_info/struct.FeeAmount.html\" title=\"struct espresso_types::v0::v0_1::fee_info::FeeAmount\">FeeAmount</a>"]]],["node_metrics",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.84.1/core/ops/arith/trait.Add.html\" title=\"trait core::ops::arith::Add\">Add</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.84.1/std/primitive.u64.html\">u64</a>&gt; for <a class=\"struct\" href=\"node_metrics/service/client_id/struct.ClientId.html\" title=\"struct node_metrics::service::client_id::ClientId\">ClientId</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[330,426]}
