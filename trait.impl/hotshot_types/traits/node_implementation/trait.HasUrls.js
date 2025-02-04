ba0e1c54083c556853643c5c45da5c096f7e3e09
jbearer <jbearer@users.noreply.github.com>
2025-02-04 16:09:37 +0000

    deploy: fc18828f027721582c5363b13ae297abc904cbea

---
+++ trait.impl/hotshot_types/traits/node_implementation/trait.HasUrls.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["espresso_types",[["impl HasUrls for <a class=\"struct\" href=\"espresso_types/v0/v0_99/auction/struct.SolverAuctionResults.html\" title=\"struct espresso_types::v0::v0_99::auction::SolverAuctionResults\">SolverAuctionResults</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[234]}
