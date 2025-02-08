53cd783741d166f61ad452b6bceda1cc11cb35dc
deepu <138998938+deepugami@users.noreply.github.com>
2025-02-08 23:51:25 +0530

    update PermissionedStakeTable.sol
    1. Added NatSpec Comments:
    Documented all functions, events, errors, and struct fields using @notice, @dev, @param, and @return tags where applicable.
    
    2. Some Other Minimal Modifications.

--- contracts/src/PermissionedStakeTable.sol
+++ contracts/src/PermissionedStakeTable.sol
@@ -6,33 +6,60 @@ import { BN254 } from "bn254/BN254.sol";
 import { EdOnBN254 } from "./libraries/EdOnBn254.sol";
 
 /**
- * @title SimpleStakeTable
- * @dev An stake table mapping with owner-only access control.
+ * @title PermissionedStakeTable
+ * @notice Manages a mapping of stakers with owner-only access control.
+ * @dev This contract allows for adding and removing stakers and ensures that only the owner can perform these operations.
  */
 contract PermissionedStakeTable is Ownable {
+    /**
+     * @dev Emitted when stakers are updated (removed or added).
+     * @param removed An array of stakers that were removed.
+     * @param added An array of new stakers that were added.
+     */
     event StakersUpdated(BN254.G2Point[] removed, NodeInfo[] added);
 
-    error StakerAlreadyExists(BN254.G2Point);
-    error StakerNotFound(BN254.G2Point);
+    /**
+     * @dev Error thrown when attempting to add a staker that already exists.
+     * @param staker The staker that already exists.
+     */
+    error StakerAlreadyExists(BN254.G2Point staker);
 
+    /**
+     * @dev Error thrown when attempting to remove a staker that does not exist.
+     * @param staker The staker that was not found.
+     */
+    error StakerNotFound(BN254.G2Point staker);
+
+    /**
+     * @notice Represents information about a node in the network.
+     * @dev Contains consensus signing keys and a flag indicating if the node is a DA node.
+     */
     struct NodeInfo {
-        /// The consensus signing key
+        /// The consensus signing key (BLS verification key).
         BN254.G2Point blsVK;
-        /// The consensus signing key. Only used for storage in this contract.
+        /// The Schnorr verification key (used for storage only).
         EdOnBN254.EdOnBN254Point schnorrVK;
-        /// Is the Node DA Node? Only used for storage in this contract.
+        /// Indicates whether the node is a DA Node (used for storage only).
         bool isDA;
     }
 
     // State mapping from staker IDs to their staking status
-    mapping(bytes32 nodeID => bool isStaker) private stakers;
+    mapping(bytes32 => bool) private stakers;
 
+    /**
+     * @notice Initializes the contract with an initial set of stakers.
+     * @param initialStakers An array of `NodeInfo` representing the initial set of stakers.
+     */
     constructor(NodeInfo[] memory initialStakers) Ownable(msg.sender) {
         insert(initialStakers);
     }
 
-    // public methods
-
+    /**
+     * @notice Updates the list of stakers by removing some and adding new ones.
+     * @dev Only callable by the owner of the contract.
+     * @param stakersToRemove An array of BLS public keys representing stakers to be removed.
+     * @param newStakers An array of `NodeInfo` representing new stakers to be added.
+     */
     function update(BN254.G2Point[] memory stakersToRemove, NodeInfo[] memory newStakers)
         public
         onlyOwner
@@ -42,10 +69,13 @@ contract PermissionedStakeTable is Ownable {
         emit StakersUpdated(stakersToRemove, newStakers);
     }
 
-    // internal methods
-
+    /**
+     * @notice Inserts new stakers into the mapping.
+     * @dev Reverts if any of the new stakers already exist or if the input array is empty.
+     * @param newStakers An array of `NodeInfo` representing new stakers to be added.
+     */
     function insert(NodeInfo[] memory newStakers) internal {
-        // TODO: revert if array empty
+        require(newStakers.length > 0, "Input array cannot be empty");
         for (uint256 i = 0; i < newStakers.length; i++) {
             bytes32 stakerID = _hashBlsKey(newStakers[i].blsVK);
             if (stakers[stakerID]) {
@@ -55,8 +85,13 @@ contract PermissionedStakeTable is Ownable {
         }
     }
 
+    /**
+     * @notice Removes existing stakers from the mapping.
+     * @dev Reverts if any of the specified stakers do not exist or if the input array is empty.
+     * @param stakersToRemove An array of BLS public keys representing stakers to be removed.
+     */
     function remove(BN254.G2Point[] memory stakersToRemove) internal {
-        // TODO: revert if array empty
+        require(stakersToRemove.length > 0, "Input array cannot be empty");
         for (uint256 i = 0; i < stakersToRemove.length; i++) {
             bytes32 stakerID = _hashBlsKey(stakersToRemove[i]);
             if (!stakers[stakerID]) {
@@ -66,12 +101,20 @@ contract PermissionedStakeTable is Ownable {
         }
     }
 
-    // view methods
-
+    /**
+     * @notice Checks if a given BLS public key belongs to an active staker.
+     * @param staker The BLS public key to check.
+     * @return True if the given key belongs to an active staker, false otherwise.
+     */
     function isStaker(BN254.G2Point memory staker) external view returns (bool) {
         return stakers[_hashBlsKey(staker)];
     }
 
+    /**
+     * @notice Computes a unique hash for a given BLS public key.
+     * @param blsVK The BLS public key to hash.
+     * @return A unique hash representing the given BLS public key.
+     */
     function _hashBlsKey(BN254.G2Point memory blsVK) public pure returns (bytes32) {
         return keccak256(abi.encode(blsVK.x0, blsVK.x1, blsVK.y0, blsVK.y1));
     }
