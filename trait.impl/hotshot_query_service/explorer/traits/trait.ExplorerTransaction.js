a5c4fd6e3a8f249ff251c361c0555f35a01ae578
sveitser <sveitser@users.noreply.github.com>
2025-02-20 14:02:50 +0000

    deploy: f20af066e225fa2a65ed578cd2a4bbc609f8f690

--- trait.impl/hotshot_query_service/explorer/traits/trait.ExplorerTransaction.js
+++ trait.impl/hotshot_query_service/explorer/traits/trait.ExplorerTransaction.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_query_service/explorer/traits/trait.ExplorerTransaction.html\" title=\"trait hotshot_query_service::explorer::traits::ExplorerTransaction\">ExplorerTransaction</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/transaction/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::transaction::Transaction\">Transaction</a>"]]],["hotshot_query_service",[]]]);
+    var implementors = Object.fromEntries([["espresso_types",[["impl ExplorerTransaction for <a class=\"struct\" href=\"espresso_types/v0/v0_1/transaction/struct.Transaction.html\" title=\"struct espresso_types::v0::v0_1::transaction::Transaction\">Transaction</a>"]]],["hotshot_query_service",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[403,29]}
+//{"start":57,"fragment_lengths":[225,29]}
--- trait.impl/serde/de/trait.Visitor.js
+++ trait.impl/serde/de/trait.Visitor.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["hotshot_query_service",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'_&gt; for <a class=\"struct\" href=\"hotshot_query_service/explorer/monetary_value/struct.MonetaryValueVisitor.html\" title=\"struct hotshot_query_service::explorer::monetary_value::MonetaryValueVisitor\">MonetaryValueVisitor</a>"]]]]);
+    var implementors = Object.fromEntries([["hotshot_query_service",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.218/serde/de/trait.Visitor.html\" title=\"trait serde::de::Visitor\">Visitor</a>&lt;'_&gt; for <a class=\"struct\" href=\"hotshot_query_service/explorer/monetary_value/struct.MonetaryValueVisitor.html\" title=\"struct hotshot_query_service::explorer::monetary_value::MonetaryValueVisitor\">MonetaryValueVisitor</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
