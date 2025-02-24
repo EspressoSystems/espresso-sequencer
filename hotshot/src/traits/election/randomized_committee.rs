9c9a62eea3d56f4f92ea122b101c375d6e789aa7
ss-es <155648797+ss-es@users.noreply.github.com>
2025-02-24 11:53:54 -0500

    add tests with fake DRB

--- hotshot/src/traits/election/randomized_committee.rs
+++ hotshot/src/traits/election/randomized_committee.rs
@@ -37,10 +37,7 @@ pub struct Committee<T: NodeType> {
     da_stake_table: Vec<<T::SignatureKey as SignatureKey>::StakeTableEntry>,
 
     /// Stake tables randomized with the DRB, used (only) for leader election
-    randomized_committees: BTreeMap<
-        <T as NodeType>::Epoch,
-        RandomizedCommittee<<T::SignatureKey as SignatureKey>::StakeTableEntry>,
-    >,
+    randomized_committee: RandomizedCommittee<<T::SignatureKey as SignatureKey>::StakeTableEntry>,
 
     /// The nodes on the committee and their stake, indexed by public key
     indexed_stake_table:
@@ -100,11 +97,14 @@ impl<TYPES: NodeType> Membership<TYPES> for Committee<TYPES> {
             .map(|entry| (TYPES::SignatureKey::public_key(entry), entry.clone()))
             .collect();
 
+        // We use a constant value of `[0u8; 32]` for the drb, since this is just meant to be used in tests
+        let randomized_committee = generate_stake_cdf(eligible_leaders.clone(), [0u8; 32]);
+
         Self {
             eligible_leaders,
             stake_table: members,
             da_stake_table: da_members,
-            randomized_committees: BTreeMap::new(),
+            randomized_committee,
             indexed_stake_table,
             indexed_da_stake_table,
         }
@@ -213,24 +213,11 @@ impl<TYPES: NodeType> Membership<TYPES> for Committee<TYPES> {
     fn lookup_leader(
         &self,
         view_number: <TYPES as NodeType>::View,
-        epoch: Option<<TYPES as NodeType>::Epoch>,
+        _epoch: Option<<TYPES as NodeType>::Epoch>,
     ) -> Result<TYPES::SignatureKey> {
-        if let Some(epoch) = epoch {
-            let Some(randomized_committee) = self.randomized_committees.get(&epoch) else {
-                bail!("Missing randomized committee for {}", epoch);
-            };
+        let res = select_randomized_leader(&self.randomized_committee, *view_number);
 
-            let res = select_randomized_leader(randomized_committee, *view_number);
-
-            Ok(TYPES::SignatureKey::public_key(&res))
-        } else {
-            #[allow(clippy::cast_possible_truncation)]
-            let index = *view_number as usize % self.eligible_leaders.len();
-
-            let res = self.eligible_leaders[index].clone();
-
-            Ok(TYPES::SignatureKey::public_key(&res))
-        }
+        Ok(TYPES::SignatureKey::public_key(&res))
     }
 
     /// Get the total number of nodes in the committee
@@ -265,10 +252,5 @@ impl<TYPES: NodeType> Membership<TYPES> for Committee<TYPES> {
         .unwrap()
     }
 
-    fn add_drb_result(&mut self, epoch: <TYPES as NodeType>::Epoch, drb: DrbResult) {
-        let randomized_committee = generate_stake_cdf(self.eligible_leaders.clone(), drb);
-
-        self.randomized_committees
-            .insert(epoch, randomized_committee);
-    }
+    fn add_drb_result(&mut self, _epoch: <TYPES as NodeType>::Epoch, _drb: DrbResult) {}
 }
