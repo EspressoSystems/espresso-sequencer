4a829de84f7e32cc023d584f6e2c3b093c1ba512
Ancient123 <Ancient123@users.noreply.github.com>
2025-02-25 16:16:24 +0000

    deploy: ab75816a8368165666729de5bb9065a22ec801d9

--- trait.impl/hotshot_types/traits/node_implementation/trait.HasUrls.js
+++ trait.impl/hotshot_types/traits/node_implementation/trait.HasUrls.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl HasUrls for <a class=\"struct\" href=\"espresso_types/v0/v0_99/struct.SolverAuctionResults.html\" title=\"struct espresso_types::v0::v0_99::SolverAuctionResults\">SolverAuctionResults</a>"]]],["hotshot_example_types",[["impl HasUrls for <a class=\"struct\" href=\"hotshot_example_types/auction_results_provider_types/struct.TestAuctionResult.html\" title=\"struct hotshot_example_types::auction_results_provider_types::TestAuctionResult\">TestAuctionResult</a>"]]]]);
+    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.HasUrls.html\" title=\"trait hotshot_types::traits::node_implementation::HasUrls\">HasUrls</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_99/struct.SolverAuctionResults.html\" title=\"struct espresso_types::v0::v0_99::SolverAuctionResults\">SolverAuctionResults</a>"]]],["hotshot_example_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.HasUrls.html\" title=\"trait hotshot_types::traits::node_implementation::HasUrls\">HasUrls</a> for <a class=\"struct\" href=\"hotshot_example_types/auction_results_provider_types/struct.TestAuctionResult.html\" title=\"struct hotshot_example_types::auction_results_provider_types::TestAuctionResult\">TestAuctionResult</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[217,273]}
+//{"start":57,"fragment_lengths":[377,433]}
--- trait.impl/hotshot_types/traits/signature_key/trait.BuilderSignatureKey.js
+++ trait.impl/hotshot_types/traits/signature_key/trait.BuilderSignatureKey.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl BuilderSignatureKey for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.FeeAccount.html\" title=\"struct espresso_types::v0::v0_1::FeeAccount\">FeeAccount</a>"]]],["hotshot_types",[]]]);
+    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/signature_key/trait.BuilderSignatureKey.html\" title=\"trait hotshot_types::traits::signature_key::BuilderSignatureKey\">BuilderSignatureKey</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.FeeAccount.html\" title=\"struct espresso_types::v0::v0_1::FeeAccount\">FeeAccount</a>"]]],["hotshot_types",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[197,21]}
+//{"start":57,"fragment_lengths":[369,21]}
