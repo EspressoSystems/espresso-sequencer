4b5c3b3197b3834439215e1245b1b097fa6c3236
Lukasz Rzasik <lukasz.rzasik@gmail.com>
2025-02-27 15:24:16 +0100

    Add test_combined_network_reup_with_epochs

--- hotshot-testing/tests/tests_6/test_epochs.rs
+++ hotshot-testing/tests/tests_6/test_epochs.rs
@@ -5,7 +5,6 @@
 // along with the HotShot repository. If not, see <https://mit-license.org/>.
 
 use std::time::Duration;
-
 use hotshot_example_types::{
     node_types::{
         CombinedImpl, EpochUpgradeTestVersions, EpochsTestVersions, Libp2pImpl, MemoryImpl,
@@ -746,6 +745,58 @@ cross_tests!(
 
         metadata.spinning_properties = SpinningTaskDescription {
             node_changes: vec![(5, all_nodes)],
+        };
+
+        metadata
+    },
+);
+
+// A run where the CDN crashes partway through
+// and then comes back up
+cross_tests!(
+    TestName: test_combined_network_reup_with_epochs,
+    Impls: [CombinedImpl],
+    Types: [TestTypes, TestTwoStakeTablesTypes],
+    Versions: [EpochsTestVersions],
+    Ignore: false,
+    Metadata: {
+        let timing_data = TimingData {
+            next_view_timeout: 10_000,
+            ..Default::default()
+        };
+
+        let overall_safety_properties = OverallSafetyPropertiesDescription {
+            num_successful_views: 35,
+            decide_timeout: Duration::from_secs(10),
+            ..Default::default()
+        };
+
+        let completion_task_description = CompletionTaskDescription::TimeBasedCompletionTaskBuilder(
+            TimeBasedCompletionTaskDescription {
+                duration: Duration::from_secs(220),
+            },
+        );
+
+        let mut metadata = TestDescription::default_multiple_rounds();
+        metadata.timing_data = timing_data;
+        metadata.overall_safety_properties = overall_safety_properties;
+        metadata.completion_task_description = completion_task_description;
+
+        let mut all_down = vec![];
+        let mut all_up = vec![];
+        for node in 0..metadata.test_config.num_nodes_with_stake.into() {
+            all_down.push(ChangeNode {
+                idx: node,
+                updown: NodeAction::NetworkDown,
+            });
+            all_up.push(ChangeNode {
+                idx: node,
+                updown: NodeAction::NetworkUp,
+            });
+        }
+
+        metadata.spinning_properties = SpinningTaskDescription {
+            node_changes: vec![(13, all_up), (5, all_down)],
         };
 
         metadata
