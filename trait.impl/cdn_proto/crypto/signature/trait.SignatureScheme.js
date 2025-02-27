11ba65b197e9387bfb176f9ebcf4d3d2ee6645d6
imabdulbasit <imabdulbasit@users.noreply.github.com>
2025-02-27 21:38:48 +0000

    deploy: 416247571940e2fa2098c4958e1f32df051d0dfd

--- trait.impl/cdn_proto/crypto/signature/trait.SignatureScheme.js
+++ trait.impl/cdn_proto/crypto/signature/trait.SignatureScheme.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hotshot",[["impl&lt;T: <a class=\"trait\" href=\"hotshot/types/trait.SignatureKey.html\" title=\"trait hotshot::types::SignatureKey\">SignatureKey</a>&gt; SignatureScheme for <a class=\"struct\" href=\"hotshot/traits/implementations/struct.WrappedSignatureKey.html\" title=\"struct hotshot::traits::implementations::WrappedSignatureKey\">WrappedSignatureKey</a>&lt;T&gt;"]]],["sequencer",[["impl&lt;T: <a class=\"trait\" href=\"hotshot_types/traits/signature_key/trait.SignatureKey.html\" title=\"trait hotshot_types::traits::signature_key::SignatureKey\">SignatureKey</a>&gt; SignatureScheme for <a class=\"struct\" href=\"sequencer/network/cdn/struct.WrappedSignatureKey.html\" title=\"struct sequencer::network::cdn::WrappedSignatureKey\">WrappedSignatureKey</a>&lt;T&gt;"]]]]);
+    var implementors = Object.fromEntries([["hotshot",[["impl&lt;T: <a class=\"trait\" href=\"hotshot/types/trait.SignatureKey.html\" title=\"trait hotshot::types::SignatureKey\">SignatureKey</a>&gt; SignatureScheme for <a class=\"struct\" href=\"hotshot/traits/implementations/struct.WrappedSignatureKey.html\" title=\"struct hotshot::traits::implementations::WrappedSignatureKey\">WrappedSignatureKey</a>&lt;T&gt;"]]],["sequencer",[["impl&lt;T: SignatureKey&gt; SignatureScheme for <a class=\"struct\" href=\"sequencer/network/cdn/struct.WrappedSignatureKey.html\" title=\"struct sequencer::network::cdn::WrappedSignatureKey\">WrappedSignatureKey</a>&lt;T&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[376,404]}
+//{"start":57,"fragment_lengths":[376,246]}
--- trait.impl/hotshot_events_service/events_source/trait.EventsSource.js
+++ trait.impl/hotshot_events_service/events_source/trait.EventsSource.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["hotshot_events_service",[]],["sequencer",[["impl&lt;N: <a class=\"trait\" href=\"hotshot_types/traits/network/trait.ConnectedNetwork.html\" title=\"trait hotshot_types::traits::network::ConnectedNetwork\">ConnectedNetwork</a>&lt;<a class=\"type\" href=\"espresso_types/v0/type.PubKey.html\" title=\"type espresso_types::v0::PubKey\">PubKey</a>&gt;, P: <a class=\"trait\" href=\"espresso_types/v0/traits/trait.SequencerPersistence.html\" title=\"trait espresso_types::v0::traits::SequencerPersistence\">SequencerPersistence</a>, V: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.Versions.html\" title=\"trait hotshot_types::traits::node_implementation::Versions\">Versions</a>&gt; <a class=\"trait\" href=\"hotshot_events_service/events_source/trait.EventsSource.html\" title=\"trait hotshot_events_service::events_source::EventsSource\">EventsSource</a>&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"sequencer/api/struct.ApiState.html\" title=\"struct sequencer::api::ApiState\">ApiState</a>&lt;N, P, V&gt;"]]]]);
+    var implementors = Object.fromEntries([["hotshot_events_service",[]],["sequencer",[["impl&lt;N: ConnectedNetwork&lt;PubKey&gt;, P: SequencerPersistence, V: Versions&gt; EventsSource&lt;SeqTypes&gt; for <a class=\"struct\" href=\"sequencer/api/struct.ApiState.html\" title=\"struct sequencer::api::ApiState\">ApiState</a>&lt;N, P, V&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[29,1128]}
+//{"start":57,"fragment_lengths":[29,271]}
