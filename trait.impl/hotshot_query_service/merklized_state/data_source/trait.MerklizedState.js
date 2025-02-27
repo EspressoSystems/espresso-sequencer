9f2f1d829f48c8fb675172ca285b815e04cd3a89
imabdulbasit <imabdulbasit@users.noreply.github.com>
2025-02-27 23:37:00 +0000

    deploy: 06c7f62ee14b7ad39163d87cdd6105a3127618c8

--- trait.impl/hotshot_query_service/merklized_state/data_source/trait.MerklizedState.js
+++ trait.impl/hotshot_query_service/merklized_state/data_source/trait.MerklizedState.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl MerklizedState&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>, { Self::ARITY }&gt; for <a class=\"type\" href=\"espresso_types/v0/v0_1/type.BlockMerkleTree.html\" title=\"type espresso_types::v0::v0_1::BlockMerkleTree\">BlockMerkleTree</a>"],["impl MerklizedState&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>, { Self::ARITY }&gt; for <a class=\"type\" href=\"espresso_types/v0/v0_1/type.FeeMerkleTree.html\" title=\"type espresso_types::v0::v0_1::FeeMerkleTree\">FeeMerkleTree</a>"]]],["hotshot_query_service",[]]]);
+    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_query_service/merklized_state/data_source/trait.MerklizedState.html\" title=\"trait hotshot_query_service::merklized_state::data_source::MerklizedState\">MerklizedState</a>&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>, { Self::ARITY }&gt; for <a class=\"type\" href=\"espresso_types/v0/v0_1/type.BlockMerkleTree.html\" title=\"type espresso_types::v0::v0_1::BlockMerkleTree\">BlockMerkleTree</a>"],["impl <a class=\"trait\" href=\"hotshot_query_service/merklized_state/data_source/trait.MerklizedState.html\" title=\"trait hotshot_query_service::merklized_state::data_source::MerklizedState\">MerklizedState</a>&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>, { Self::ARITY }&gt; for <a class=\"type\" href=\"espresso_types/v0/v0_1/type.FeeMerkleTree.html\" title=\"type espresso_types::v0::v0_1::FeeMerkleTree\">FeeMerkleTree</a>"]]],["hotshot_query_service",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[678,29]}
+//{"start":57,"fragment_lengths":[1062,29]}
