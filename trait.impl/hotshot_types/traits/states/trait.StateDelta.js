0d622e7846f54edc630c8603279ddaf691bcc377
imabdulbasit <imabdulbasit@users.noreply.github.com>
2025-03-04 01:25:04 +0000

    deploy: 966193042b36e816e045489a6a8373f2d52aa899

--- trait.impl/hotshot_types/traits/states/trait.StateDelta.js
+++ trait.impl/hotshot_types/traits/states/trait.StateDelta.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl StateDelta for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Delta.html\" title=\"struct espresso_types::v0::v0_1::Delta\">Delta</a>"]]],["hotshot_example_types",[["impl StateDelta for <a class=\"struct\" href=\"hotshot_example_types/state_types/struct.TestStateDelta.html\" title=\"struct hotshot_example_types::state_types::TestStateDelta\">TestStateDelta</a>"]]]]);
+    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/states/trait.StateDelta.html\" title=\"trait hotshot_types::traits::states::StateDelta\">StateDelta</a> for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Delta.html\" title=\"struct espresso_types::v0::v0_1::Delta\">Delta</a>"]]],["hotshot_example_types",[["impl <a class=\"trait\" href=\"hotshot_types/traits/states/trait.StateDelta.html\" title=\"trait hotshot_types::traits::states::StateDelta\">StateDelta</a> for <a class=\"struct\" href=\"hotshot_example_types/state_types/struct.TestStateDelta.html\" title=\"struct hotshot_example_types::state_types::TestStateDelta\">TestStateDelta</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[173,229]}
+//{"start":57,"fragment_lengths":[313,369]}
--- trait.impl/hotshot_types/traits/storage/trait.Storage.js
+++ trait.impl/hotshot_types/traits/storage/trait.Storage.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl&lt;P: <a class=\"trait\" href=\"espresso_types/v0/traits/trait.SequencerPersistence.html\" title=\"trait espresso_types::v0::traits::SequencerPersistence\">SequencerPersistence</a>&gt; Storage&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"https://doc.rust-lang.org/1.85.0/alloc/sync/struct.Arc.html\" title=\"struct alloc::sync::Arc\">Arc</a>&lt;P&gt;"]]],["hotshot_example_types",[["impl&lt;TYPES: NodeType&gt; Storage&lt;TYPES&gt; for <a class=\"struct\" href=\"hotshot_example_types/storage_types/struct.TestStorage.html\" title=\"struct hotshot_example_types::storage_types::TestStorage\">TestStorage</a>&lt;TYPES&gt;"]]]]);
+    var implementors = Object.fromEntries([["espresso_types",[["impl&lt;P: <a class=\"trait\" href=\"espresso_types/v0/traits/trait.SequencerPersistence.html\" title=\"trait espresso_types::v0::traits::SequencerPersistence\">SequencerPersistence</a>&gt; <a class=\"trait\" href=\"hotshot_types/traits/storage/trait.Storage.html\" title=\"trait hotshot_types::traits::storage::Storage\">Storage</a>&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"https://doc.rust-lang.org/1.85.0/alloc/sync/struct.Arc.html\" title=\"struct alloc::sync::Arc\">Arc</a>&lt;P&gt;"]]],["hotshot_example_types",[["impl&lt;TYPES: <a class=\"trait\" href=\"hotshot_types/traits/node_implementation/trait.NodeType.html\" title=\"trait hotshot_types::traits::node_implementation::NodeType\">NodeType</a>&gt; <a class=\"trait\" href=\"hotshot_types/traits/storage/trait.Storage.html\" title=\"trait hotshot_types::traits::storage::Storage\">Storage</a>&lt;TYPES&gt; for <a class=\"struct\" href=\"hotshot_example_types/storage_types/struct.TestStorage.html\" title=\"struct hotshot_example_types::storage_types::TestStorage\">TestStorage</a>&lt;TYPES&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[500,270]}
+//{"start":57,"fragment_lengths":[636,568]}
