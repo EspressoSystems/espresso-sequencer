55ea518658a46e8004f6a189fc159fc441a000e0
imabdulbasit <baronbasit@gmail.com>
2025-03-05 15:21:48 +0500

    do not overwrite existing payload with empty payload

--- hotshot-query-service/src/data_source/storage/sql/transaction.rs
+++ hotshot-query-service/src/data_source/storage/sql/transaction.rs
@@ -496,9 +496,10 @@ where
         // Similarly, we can initialize the payload table with a null payload, which can help us
         // distinguish between blocks that haven't been produced yet and blocks we haven't received
         // yet when answering queries.
-        self.upsert("payload", ["height"], ["height"], [(height as i64,)])
-            .await?;
-
+        let query = query("INSERT INTO payload (height) VALUES ($1) ON CONFLICT DO NOTHING")
+            .bind(height as i64);
+        query.execute(self.as_mut()).await?;
+        
         // Finally, we insert the leaf itself, which references the header row we created.
         // Serialize the full leaf and QC to JSON for easy storage.
         let leaf_json = serde_json::to_value(leaf.leaf()).context("failed to serialize leaf")?;
