e93f3a602daf37e383f66cd93cc3cc6867cb7851
sveitser <sveitser@users.noreply.github.com>
2025-03-06 14:38:49 +0000

    deploy: 5d6c9669d816988b96cd113894118447fbc09a37

--- trait.impl/hotshot_query_service/availability/query_data/trait.QueryablePayload.js
+++ trait.impl/hotshot_query_service/availability/query_data/trait.QueryablePayload.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["espresso_types",[["impl QueryablePayload&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Payload.html\" title=\"struct espresso_types::v0::v0_1::Payload\">Payload</a>"]]],["hotshot_query_service",[]]]);
+    var implementors = Object.fromEntries([["espresso_types",[["impl <a class=\"trait\" href=\"hotshot_query_service/availability/query_data/trait.QueryablePayload.html\" title=\"trait hotshot_query_service::availability::query_data::QueryablePayload\">QueryablePayload</a>&lt;<a class=\"struct\" href=\"espresso_types/v0/struct.SeqTypes.html\" title=\"struct espresso_types::v0::SeqTypes\">SeqTypes</a>&gt; for <a class=\"struct\" href=\"espresso_types/v0/v0_1/struct.Payload.html\" title=\"struct espresso_types::v0::v0_1::Payload\">Payload</a>"]]],["hotshot_query_service",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[319,29]}
+//{"start":57,"fragment_lengths":[507,29]}
