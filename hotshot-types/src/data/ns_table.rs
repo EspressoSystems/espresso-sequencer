2ee151328da8753aece506dad42782529b083e8e
Chengyu Lin <linmrain@gmail.com>
2025-03-07 10:39:16 -0500

    Fix new vid panic on empty payload (#2733)
    * fix panic on empty payload
    
    * better fix
    
    * typo

--- hotshot-types/src/data/ns_table.rs
+++ hotshot-types/src/data/ns_table.rs
@@ -29,8 +29,13 @@ pub fn parse_ns_table(payload_byte_len: usize, bytes: &[u8]) -> Vec<Range<usize>
     if num_entries
         != bytes.len().saturating_sub(NUM_NSS_BYTE_LEN)
             / NS_ID_BYTE_LEN.saturating_add(NS_OFFSET_BYTE_LEN)
+        || (num_entries == 0 && payload_byte_len != 0)
     {
         tracing::warn!("Failed to parse the metadata as namespace table. Use a single namespace table instead.");
+        return vec![(0..payload_byte_len)];
+    }
+    // Early breaks for empty payload and namespace table
+    if num_entries == 0 {
         return vec![(0..payload_byte_len)];
     }
     let mut l = 0;
